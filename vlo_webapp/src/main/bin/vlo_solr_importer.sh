c5426ecc132f66bb0463cb047e19d0e1a04f1965
Patrick Duin <Patrick.Duin@mpi.nl>
2010-10-07 07:27:38 +0000

    * created import build goal so I can run a script

---
+++ vlo_webapp/src/main/bin/vlo_solr_importer.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+LIB=../share
+CLASSPATH=.:./log4j.properties
+JAVA=java
+
+for f in `ls $LIB`
+do
+	CLASSPATH=$CLASSPATH:$LIB/$f
+done
+
+#echo $CLASSPATH
+
+$JAVA -Xmx1024M -cp $CLASSPATH eu.clarin.cmdi.vlo.importer.MetadataImporter "$@"
