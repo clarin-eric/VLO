a2e64cd1792e68ff9bf47429e6edd603f85d0b97
Patrick Duin <Patrick.Duin@mpi.nl>
2010-12-15 12:48:10 +0000

    * Added vtd-xml parser which is 10x faster (5mins vs. 1hour for 200.000 cmdi files) then DOM parser. Still supports xpath to query the data out. * Made it possible to delete only part of the index, you can delete a whole origin.

--- vlo_webapp/src/main/java/eu/clarin/cmdi/vlo/importer/DataRoot.java
+++ vlo_webapp/src/main/java/eu/clarin/cmdi/vlo/importer/DataRoot.java
@@ -7,6 +7,7 @@ public class DataRoot {
     private FacetMapping facetMapping;
     private String originName;
     private File rootFile;
+    private boolean deleteFirst = false;
 
     public void setFacetMapping(FacetMapping facetMapping) {
         this.facetMapping = facetMapping;
@@ -30,6 +31,14 @@ public class DataRoot {
     
     public void setRootFile(File rootFile) {
         this.rootFile = rootFile;
+    }
+
+    public void setDeleteFirst(boolean deleteFirst) {
+        this.deleteFirst = deleteFirst;
+    }
+
+    public boolean isDeleteFirst() {
+        return deleteFirst;
     }
 
 }
