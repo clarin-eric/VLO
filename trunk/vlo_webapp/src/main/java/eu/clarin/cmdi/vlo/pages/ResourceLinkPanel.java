330a44ed83d84c3f737140092b0f8d2ae43d5325
Patrick Duin <Patrick.Duin@mpi.nl>
2011-01-10 13:53:20 +0000

    * fixed resource link when it is nort a handle

--- trunk/vlo_webapp/src/main/java/eu/clarin/cmdi/vlo/pages/ResourceLinkPanel.java
+++ trunk/vlo_webapp/src/main/java/eu/clarin/cmdi/vlo/pages/ResourceLinkPanel.java
@@ -1,5 +1,7 @@
 package eu.clarin.cmdi.vlo.pages;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,12 +10,16 @@ import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.resource.ContextRelativeResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import eu.clarin.cmdi.vlo.CommonUtils;
 import eu.clarin.cmdi.vlo.Configuration;
 import eu.clarin.cmdi.vlo.FacetConstants;
 
 public class ResourceLinkPanel extends Panel {
+    private final static Logger LOG = LoggerFactory.getLogger(ResourceLinkPanel.class);
+
     private static final long serialVersionUID = 1L;
 
     private final static ImageResource ANNOTATION = new ImageResource(new ContextRelativeResource("Images/text-x-log.png"),
@@ -22,6 +28,7 @@ public class ResourceLinkPanel extends Panel {
     private final static ImageResource IMAGE = new ImageResource(new ContextRelativeResource("Images/image-x-generic.png"), "Image file");
     private final static ImageResource TEXT = new ImageResource(new ContextRelativeResource("Images/text-x-generic.png"), "Text file");
     private final static ImageResource VIDEO = new ImageResource(new ContextRelativeResource("Images/video-x-generic.png"), "Video file");
+    private static final String HANDLE_PREFIX = "hdl";
 
     private final static Map<String, ImageResource> ICON_MAP = new HashMap<String, ImageResource>();
     static {
@@ -40,7 +47,23 @@ public class ResourceLinkPanel extends Panel {
         resourceImg.add(new SimpleAttributeModifier("title", title));
         resourceImg.add(new SimpleAttributeModifier("alt", title));
         add(resourceImg);
-        add(new ExternalLink("resourceLink", Configuration.getInstance().getHandleServerUrl() + resourceLink, resourceLink));
+        String href = getHref(resourceLink);
+        add(new ExternalLink("resourceLink", href, resourceLink));
+    }
+
+    private String getHref(String resourceLink) {
+        String result = resourceLink;
+        try {
+            URL url = new URL(resourceLink);
+            String protocol = url.getProtocol();
+            if (HANDLE_PREFIX.equalsIgnoreCase(protocol)) {
+                result = Configuration.getInstance().getHandleServerUrl() + resourceLink;
+            }
+        } catch (MalformedURLException e) {
+            LOG.debug("URL error", e);
+            //ignore exception, just show the original link, perhaps the user can do something with, we cannot do anything else.
+        }
+        return result;
     }
 
     private ImageResource getImage(String mimeType) {
