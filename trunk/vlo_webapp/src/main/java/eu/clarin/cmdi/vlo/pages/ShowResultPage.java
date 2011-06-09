febe1ae94958e8192ba6473e3e3f67f2b98f8db2
Patrick Duin <Patrick.Duin@mpi.nl>
2011-06-09 11:43:07 +0000

    Ticket 131: Showing proper error page when id cannot be loaded and redirected to searchpage keeping the given search parameters.

--- trunk/vlo_webapp/src/main/java/eu/clarin/cmdi/vlo/pages/ShowResultPage.java
+++ trunk/vlo_webapp/src/main/java/eu/clarin/cmdi/vlo/pages/ShowResultPage.java
@@ -44,126 +44,131 @@ public class ShowResultPage extends BasePage {
 
     @SuppressWarnings("serial")
     public ShowResultPage(final PageParameters parameters) {
-        super(parameters);
-        final String docId = WicketURLDecoder.QUERY_INSTANCE.decode(getPageParameters().getString(PARAM_DOC_ID, null));
-        final SearchPageQuery query = new SearchPageQuery(parameters);
-        BookmarkablePageLink backLink = new BookmarkablePageLink("backLink", FacetedSearchPage.class, query.getPageParameters());
-        add(backLink);
-        String href = getHref(docId);
-        if (href != null) {
-            add(new ExternalLink("openBrowserLink", href, new ResourceModel(Resources.OPEN_IN_ORIGINAL_CONTEXT).getObject()));
-        } else {
-            add(new Label("openBrowserLink", new ResourceModel(Resources.ORIGINAL_CONTEXT_NOT_AVAILABLE).getObject()));
-        }
-        add(new AjaxLazyLoadPanel("prevNextHeader") {
+	super(parameters);
+	final String docId = WicketURLDecoder.QUERY_INSTANCE.decode(getPageParameters().getString(PARAM_DOC_ID, null));
+	SolrDocument solrDocument = DaoLocator.getSearchResultsDao().getSolrDocument(docId);
+	if (solrDocument != null) {
+	    final SearchPageQuery query = new SearchPageQuery(parameters);
+	    BookmarkablePageLink backLink = new BookmarkablePageLink("backLink", FacetedSearchPage.class, query.getPageParameters());
+	    add(backLink);
+	    String href = getHref(docId);
+	    if (href != null) {
+		add(new ExternalLink("openBrowserLink", href, new ResourceModel(Resources.OPEN_IN_ORIGINAL_CONTEXT).getObject()));
+	    } else {
+		add(new Label("openBrowserLink", new ResourceModel(Resources.ORIGINAL_CONTEXT_NOT_AVAILABLE).getObject()));
+	    }
+	    add(new AjaxLazyLoadPanel("prevNextHeader") {
 
-            @Override
-            public Component getLazyLoadComponent(String markupId) {
-                return new PrevNextHeaderPanel(markupId, docId, query);
-            }
+		@Override
+		public Component getLazyLoadComponent(String markupId) {
+		    return new PrevNextHeaderPanel(markupId, docId, query);
+		}
 
-            @Override
-            public Component getLoadingComponent(String markupId) {
-                return new PrevNextHeaderPanel(markupId);
-            }
-        });
+		@Override
+		public Component getLoadingComponent(String markupId) {
+		    return new PrevNextHeaderPanel(markupId);
+		}
+	    });
 
-        SolrDocument solrDocument = DaoLocator.getSearchResultsDao().getSolrDocument(docId);
-        addAttributesTable(solrDocument);
-        addResourceLinks(solrDocument);
+	    addAttributesTable(solrDocument);
+	    addResourceLinks(solrDocument);
+	} else {
+	    setResponsePage(new ResultNotFoundPage(parameters));
+	}
+
     }
 
     private String getHref(String linkToOriginalContext) {
-        String result = linkToOriginalContext;
-        if (linkToOriginalContext != null) {
-            if (linkToOriginalContext.startsWith(FacetConstants.TEST_HANDLE_PREFIX)) {
-                linkToOriginalContext = linkToOriginalContext.replace(FacetConstants.TEST_HANDLE_PREFIX, FacetConstants.HANDLE_PREFIX);
-            }
-            if (linkToOriginalContext.startsWith(FacetConstants.HANDLE_PREFIX)) {
-                result = Configuration.getInstance().getIMDIBrowserUrl(linkToOriginalContext);
-            } else {
-                try {
-                    new URL(linkToOriginalContext);
-                } catch (MalformedURLException e) {
-                    LOG.debug("Link to original context is incorrect:", e);
-                    result = null;
-                }
-            }
-        }
-        return result;
+	String result = linkToOriginalContext;
+	if (linkToOriginalContext != null) {
+	    if (linkToOriginalContext.startsWith(FacetConstants.TEST_HANDLE_PREFIX)) {
+		linkToOriginalContext = linkToOriginalContext.replace(FacetConstants.TEST_HANDLE_PREFIX, FacetConstants.HANDLE_PREFIX);
+	    }
+	    if (linkToOriginalContext.startsWith(FacetConstants.HANDLE_PREFIX)) {
+		result = Configuration.getInstance().getIMDIBrowserUrl(linkToOriginalContext);
+	    } else {
+		try {
+		    new URL(linkToOriginalContext);
+		} catch (MalformedURLException e) {
+		    LOG.debug("Link to original context is incorrect:", e);
+		    result = null;
+		}
+	    }
+	}
+	return result;
     }
 
     private void addAttributesTable(final SolrDocument solrDocument) {
-        DocumentAttributesDataProvider attributeProvider = new DocumentAttributesDataProvider(solrDocument);
-        DataTable table = new DataTable("attributesTable", createAttributesColumns(), attributeProvider, 250);
-        table.setTableBodyCss("attributesTbody");
-        table.addTopToolbar(new HeadersToolbar(table, null));
-        add(table);
+	DocumentAttributesDataProvider attributeProvider = new DocumentAttributesDataProvider(solrDocument);
+	DataTable table = new DataTable("attributesTable", createAttributesColumns(), attributeProvider, 250);
+	table.setTableBodyCss("attributesTbody");
+	table.addTopToolbar(new HeadersToolbar(table, null));
+	add(table);
     }
 
     @SuppressWarnings("serial")
     private IColumn[] createAttributesColumns() {
-        IColumn[] columns = new IColumn[2];
+	IColumn[] columns = new IColumn[2];
 
-        columns[0] = new PropertyColumn(new ResourceModel(Resources.FIELD), "field") {
+	columns[0] = new PropertyColumn(new ResourceModel(Resources.FIELD), "field") {
 
-            @Override
-            public String getCssClass() {
-                return "attribute";
-            }
-        };
-        columns[1] = new AbstractColumn<DocumentAttribute>(new ResourceModel(Resources.VALUE)) {
+	    @Override
+	    public String getCssClass() {
+		return "attribute";
+	    }
+	};
+	columns[1] = new AbstractColumn<DocumentAttribute>(new ResourceModel(Resources.VALUE)) {
 
-            @Override
-            public void populateItem(Item<ICellPopulator<DocumentAttribute>> cellItem, String componentId,
-                    IModel<DocumentAttribute> rowModel) {
-                DocumentAttribute attribute = rowModel.getObject();
-                cellItem.add(new SmartLinkMultiLineLabel(componentId, attribute.getValue()) {
+	    @Override
+	    public void populateItem(Item<ICellPopulator<DocumentAttribute>> cellItem, String componentId,
+		    IModel<DocumentAttribute> rowModel) {
+		DocumentAttribute attribute = rowModel.getObject();
+		cellItem.add(new SmartLinkMultiLineLabel(componentId, attribute.getValue()) {
 
-                    @Override
-                    protected void onComponentTagBody(MarkupStream markupStream, ComponentTag openTag) {
-                        CharSequence body = StringUtils.toMultiLineHtml(getDefaultModelObjectAsString());
-                        replaceComponentTagBody(markupStream, openTag, getSmartLink(body));
-                    }
-                });
-            }
+		    @Override
+		    protected void onComponentTagBody(MarkupStream markupStream, ComponentTag openTag) {
+			CharSequence body = StringUtils.toMultiLineHtml(getDefaultModelObjectAsString());
+			replaceComponentTagBody(markupStream, openTag, getSmartLink(body));
+		    }
+		});
+	    }
 
-            @Override
-            public String getCssClass() {
-                return "attributeValue";
-            }
-        };
-        return columns;
+	    @Override
+	    public String getCssClass() {
+		return "attributeValue";
+	    }
+	};
+	return columns;
     }
 
     @SuppressWarnings("serial")
     private void addResourceLinks(SolrDocument solrDocument) {
-        RepeatingView repeatingView = new RepeatingView("resourceList");
-        add(repeatingView);
-        if (solrDocument.containsKey(FacetConstants.FIELD_RESOURCE)) {
-            Collection<Object> resources = solrDocument.getFieldValues(FacetConstants.FIELD_RESOURCE);
-            for (Object resource : resources) {
-                String[] split = resource.toString().split(Pattern.quote(FacetConstants.FIELD_RESOURCE_SPLIT_CHAR), 2);
-                final String mimeType = split[0];
-                final String resourceLink = split[1];
-                repeatingView.add(new AjaxLazyLoadPanel(repeatingView.newChildId()) {
-                    @Override
-                    public Component getLazyLoadComponent(String markupId) {
-                        return new ResourceLinkPanel(markupId, mimeType, resourceLink);
-                    }
+	RepeatingView repeatingView = new RepeatingView("resourceList");
+	add(repeatingView);
+	if (solrDocument.containsKey(FacetConstants.FIELD_RESOURCE)) {
+	    Collection<Object> resources = solrDocument.getFieldValues(FacetConstants.FIELD_RESOURCE);
+	    for (Object resource : resources) {
+		String[] split = resource.toString().split(Pattern.quote(FacetConstants.FIELD_RESOURCE_SPLIT_CHAR), 2);
+		final String mimeType = split[0];
+		final String resourceLink = split[1];
+		repeatingView.add(new AjaxLazyLoadPanel(repeatingView.newChildId()) {
+		    @Override
+		    public Component getLazyLoadComponent(String markupId) {
+			return new ResourceLinkPanel(markupId, mimeType, resourceLink);
+		    }
 
-                });
-            }
-        } else {
-            repeatingView.add(new Label(repeatingView.newChildId(), new ResourceModel(Resources.NO_RESOURCE_FOUND)));
-        }
+		});
+	    }
+	} else {
+	    repeatingView.add(new Label(repeatingView.newChildId(), new ResourceModel(Resources.NO_RESOURCE_FOUND)));
+	}
     }
 
     public static BookmarkablePageLink<ShowResultPage> createBookMarkableLink(String linkId, SearchPageQuery query, String docId) {
-        PageParameters pageParameters = query.getPageParameters();
-        pageParameters.put(ShowResultPage.PARAM_DOC_ID, WicketURLEncoder.QUERY_INSTANCE.encode(docId));
-        BookmarkablePageLink<ShowResultPage> docLink = new BookmarkablePageLink<ShowResultPage>(linkId, ShowResultPage.class,
-                pageParameters);
-        return docLink;
+	PageParameters pageParameters = query.getPageParameters();
+	pageParameters.put(ShowResultPage.PARAM_DOC_ID, WicketURLEncoder.QUERY_INSTANCE.encode(docId));
+	BookmarkablePageLink<ShowResultPage> docLink = new BookmarkablePageLink<ShowResultPage>(linkId, ShowResultPage.class,
+		pageParameters);
+	return docLink;
     }
 }
