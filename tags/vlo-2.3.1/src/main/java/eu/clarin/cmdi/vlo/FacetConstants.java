fbe06eae271c3a0e5fc0f8353e5fc3224d87f539
Patrick Duin <Patrick.Duin@mpi.nl>
2011-06-28 10:06:35 +0000

    * disabled handle resolving, doesn't work in clarin server see ticket: http://trac.clarin.eu/ticket/136 patched version to 2.3.1

---
+++ tags/vlo-2.3.1/src/main/java/eu/clarin/cmdi/vlo/FacetConstants.java
@@ -0,0 +1,36 @@
+package eu.clarin.cmdi.vlo;
+
+public class FacetConstants {
+
+    public static final String FIELD_NAME = "name";
+    public static final String FIELD_ID = "id";
+    public static final String FIELD_DATA_PROVIDER = "dataProvider";
+    public static final String FIELD_DESCRIPTION = "description";
+    public static final String FIELD_COLLECTION = "collection";
+    public static final String FIELD_COUNTRY = "country";
+    public static final String FIELD_CONTINENT = "continent";
+    public static final String FIELD_LANGUAGE = "language";
+    public static final String FIELD_YEAR = "year";
+    public static final String FIELD_GENRE = "genre";
+    public static final String FIELD_SUBJECT = "subject";
+    public static final String FIELD_ORGANISATION = "organisation";
+    public static final String FIELD_RESOURCE_TYPE = "resourceType"; 
+    public static final String FIELD_PROJECT_NAME = "projectName";
+
+    //The _ facets are not meant to be shown to users.
+    public static final String FIELD_FILENAME = "_fileName";
+    public static final String FIELD_RESOURCE = "_resourceRef";
+    
+    //Normalized mimeTypes
+    public static final String RESOURCE_TYPE_AUDIO = "audio";
+    public static final String RESOURCE_TYPE_VIDEO = "video";
+    public static final String RESOURCE_TYPE_TEXT = "text";
+    public static final String RESOURCE_TYPE_IMAGE = "image";
+    public static final String RESOURCE_TYPE_ANNOTATION = "annotation";
+    
+    public static final String HANDLE_MPI_PREFIX = "hdl:1839";
+    public static final String HANDLE_PREFIX = "hdl:";
+    public static final String TEST_HANDLE_MPI_PREFIX = "test-hdl:1839";
+    public static final String FIELD_RESOURCE_SPLIT_CHAR = "|";
+
+}
---
+++ tags/vlo-2.3.1/src/main/java/eu/clarin/cmdi/vlo/pages/PrevNextHeaderPanel.java
@@ -0,0 +1,75 @@
+package eu.clarin.cmdi.vlo.pages;
+
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.panel.Panel;
+
+import eu.clarin.cmdi.vlo.FacetConstants;
+import eu.clarin.cmdi.vlo.dao.DaoLocator;
+
+public class PrevNextHeaderPanel extends Panel {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Creates Prev/Next Header can take a while to generate
+     * @param id
+     * @param docId
+     * @param query
+     */
+    public PrevNextHeaderPanel(String id, String docId, SearchPageQuery query) {
+        super(id);
+        addPrevNextLabels(docId, query);
+    }
+
+    /**
+     * Constructor that creates dummy prev/next label, which can be used to load before the real labels are calculated.
+     * @param id
+     */
+    public PrevNextHeaderPanel(String id) {
+        super(id);
+        addDummyPrev();
+        addDummyNext();
+    }
+
+    private void addDummyPrev() {
+        add(new WebMarkupContainer("prev"));
+    }
+
+    private void addDummyNext() {
+        add(new WebMarkupContainer("next"));
+    }
+
+    private void addPrevNextLabels(String docId, SearchPageQuery query) {
+        int index = -1;
+        //Very inefficient query, possibly gets all the docId's (when no facets are selected) just to create the next/prev labels. 
+        //Cannot see another way of doing this because the result page can be bookmarked and therefor the list needs to be refetched. 
+        //If some facets are selected the list of id's is already a lot smaller and performance is not an issue. I lazy loaded the panel just to everything a bit more snappy.
+        SolrDocumentList docIdList = DaoLocator.getSearchResultsDao().getDocIdList(query.getSolrQuery().getCopy());  
+
+        for (int i = 0; i < docIdList.size(); i++) {
+            SolrDocument doc = docIdList.get(i);
+            if (doc.getFieldValue(FacetConstants.FIELD_ID).equals(docId)) {
+                index = i;
+                break;
+            }
+        }
+        if (index > 0) {
+            String prevDocId = docIdList.get(index - 1).getFieldValue(FacetConstants.FIELD_ID).toString();
+            BookmarkablePageLink<ShowResultPage> prev = ShowResultPage.createBookMarkableLink("prev", query, prevDocId);
+            add(prev);
+        } else {
+            addDummyPrev();
+        }
+        if (index < (docIdList.size() - 1) && index >= 0) {
+            String prevDocId = docIdList.get(index + 1).getFieldValue(FacetConstants.FIELD_ID).toString();
+            BookmarkablePageLink<ShowResultPage> next = ShowResultPage.createBookMarkableLink("next", query, prevDocId);
+            add(next);
+        } else {
+            addDummyNext();
+        }
+    }
+
+}
