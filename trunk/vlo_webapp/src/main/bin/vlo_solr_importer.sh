a0fc901bb37a7abbd13c36ee8bdecfc86ab8be73
hermanstehouwer <j.h.stehouwer@gmail.com>
2012-04-17 07:26:22 +0000

    small annoyance fixed: no more daily 'can't find java' mails for root

--- trunk/vlo_webapp/src/main/bin/vlo_solr_importer.sh
+++ trunk/vlo_webapp/src/main/bin/vlo_solr_importer.sh
@@ -5,7 +5,9 @@
 WORKDIR=.
 LIB=../share
 CLASSPATH=.
-JAVA=`which java`
+
+# Filter of the Error stream from which to prevent the admins from getting daily "can't find java" emails
+JAVA=`which java 2>/dev/null`
 if [  -z $JAVA ]; then
 #set java for mpi servers
 JAVA=/lat/java/bin/java
