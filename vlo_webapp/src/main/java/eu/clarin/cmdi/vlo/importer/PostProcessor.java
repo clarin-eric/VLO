f6264813aa899bbc2fbbda3fccd22303ac814d92
hermanstehouwer <j.h.stehouwer@gmail.com>
2012-11-27 14:10:16 +0000

    some comments

--- vlo_webapp/src/main/java/eu/clarin/cmdi/vlo/importer/PostProcessor.java
+++ vlo_webapp/src/main/java/eu/clarin/cmdi/vlo/importer/PostProcessor.java
@@ -1,5 +1,13 @@
 package eu.clarin.cmdi.vlo.importer;
 
+/**
+ * Defines the interface for a postprocessor.
+ *
+ * Such a postprossor is called on a single facet value after this facet value is inserted into the solr document during import.
+ *
+ * at the start of MetadataImporter which postprocessors (if any) are used for which facet are defined.
+ */
+
 public interface PostProcessor {
 
     public String process(String value);
