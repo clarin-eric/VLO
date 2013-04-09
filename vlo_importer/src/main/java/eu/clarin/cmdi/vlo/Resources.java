71b742eb8449312014b30545ee8277a62c182b68
kjvandelooij <keeloo@mpi.nl>
2013-04-09 13:49:26 +0000

    Added landing page key values to the Resources.java file.

--- vlo_importer/src/main/java/eu/clarin/cmdi/vlo/Resources.java
+++ vlo_importer/src/main/java/eu/clarin/cmdi/vlo/Resources.java
@@ -1,21 +1,31 @@
 package eu.clarin.cmdi.vlo;
 
-
 /**
- * Constants for i18n resources.
- * See VloApplication.properties for values belong to the "key" constants 
- * Can be used in java but also in the wicket html files e.g.: <wicket:message key="prev"></wicket:message>
+ * Definition of keys to message texts.<br><br>
+ * 
+ * Please note that value of a key defined in this class is defined in the
+ * VloWebApplication.properties file. The key value pairs can be used in HTML
+ * pages that are subject to Wicket, for example:<br><br>
+ *
+ * {@literal <div class="resourceList"><span>}
+ * {@literal    <wicket:message key="resources">[Resources]</wicket:message>:</span>}
+ * .
+ * .
+ * .
+ * {@literal </div>}
  */
 public interface Resources {
     
     public static final String FIELD = "field";
-    public final static String NO_RESOURCE_FOUND = "noResourceFound";
+    public static final String NO_RESOURCE_FOUND = "noResourceFound";
     public static final String VALUE = "value";
     public static final String RESULTS = "results";
     public static final String NAME = "name";
     public static final String DESCRIPTION = "description";
     public static final String OPEN_IN_ORIGINAL_CONTEXT = "openInOriginalContext";
-    public static final String ORIGINAL_CONTEXT_NOT_AVAILABLE = "originalContextNotAvailable";    
-    
+    public static final String ORIGINAL_CONTEXT_NOT_AVAILABLE = "originalContextNotAvailable";  
 
+    // keys to text used in labels in the page showing search result
+    public static final String LANDING_PAGE = "landingPage";    
+    public static final String LANDING_PAGES = "landingPages";    
 }
