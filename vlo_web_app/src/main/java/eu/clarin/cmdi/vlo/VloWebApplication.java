dd4bad3413eea13301091cc2725bb84e47c2873e
kjvandelooij <keeloo@mpi.nl>
2013-04-22 13:18:09 +0000

    Repaired the documentation in VloWebApplication reffering to the VLO configuration.

--- vlo_web_app/src/main/java/eu/clarin/cmdi/vlo/VloWebApplication.java
+++ vlo_web_app/src/main/java/eu/clarin/cmdi/vlo/VloWebApplication.java
@@ -8,24 +8,28 @@ import javax.servlet.ServletContext;
 import org.apache.wicket.protocol.http.WebApplication;
 
 /**
- * {@literal VLO} web application.<br><br>
+ * {@literal VLO} web application.
  *
+ * The web application class defines an object representing the VLO web
+ * application. 
+ * 
  * Because the VloWebApplication class extends WebApplication, a class instance
- * will normally reside inside a web server container. By running the Start
- * class however, an instance of the application will reside outside a server
+ * will reside inside a web server container. By running the Start class
+ * however, an instance of the application will reside outside a server
  * container.
- *
  */
 public class VloWebApplication extends WebApplication {
 
     private SearchResultsDao searchResults;
     
-    // flag indicating whether or not the application object lives in a context
+    /**
+     * Flag indicating whether or not the application object lives in a web
+     * server context.
+     */
     boolean inContext;
 
     /**
-     * Method that will be invoked when the application starts<br><br>
-     *
+     * Method that will be invoked when the application starts.
      */
     @Override
     public void init() {
@@ -37,7 +41,7 @@ public class VloWebApplication extends WebApplication {
             ServletContext servletContext;
             servletContext = this.getServletContext();
             
-            /**
+            /*
              * Send the application context to the configuration object to
              * enable it to read an external {@literal VloConfig.xml}
              * configuration file.
@@ -52,14 +56,14 @@ public class VloWebApplication extends WebApplication {
     }
 
     /**
-     * Web application constructor<br><br>
+     * Web application constructor.
      *
      * Create an application instance configured to be living inside a web
      * server container.
      */
     public VloWebApplication() {
 
-        /**
+        /*
          * Read the application's packaged configuration. 
          * 
          * Because on instantiation a web application cannot said to be living 
@@ -76,28 +80,24 @@ public class VloWebApplication extends WebApplication {
     }
 
     /**
-     * Web application constructor<br><br>
+     * Web application constructor.
      *
-     * Create an application instance configured to be living without a web
-     * server container context.<br><br>
-     *
-     * @param testConfig a configuration that could be different from the
-     * packaged parameters
-     *
-     * An instance like this can for example be used for testing purposes. In
-     * the case of testing, the constructor is invoked from a class in a test
-     * package. Within such a class a configuration object can be manipulated
-     * according to specific needs. <br><br>
-     *
-     * Please note that in the case of a test configuration, while the
-     * application object could reside inside a web server container, the
-     * context associated with this container will be ignored.
+     * Allows for the creation of an application instance that does not rely on
+     * a web server context. When send the message 'false', this constructor
+     * will create an object that will not look for an external configuration
+     * file; it will exclusively rely on the packaged configuration. Typically,
+     * the application's tests will send false to the application constructor.
+     * 
+     * @param inContext If and only if this parameter equals true. later on, the
+     * {@literal init} method will try to determine the web server's container 
+     * context so that, if it is defined in it, an external configuration can be
+     * switched to. 
      */
-    public VloWebApplication(Boolean param) {
+    public VloWebApplication(Boolean inContext) {
 
         // remember that the application does not live in a web server context
         
-        inContext = param;
+        this.inContext = inContext;
 
         searchResults = new SearchResultsDao();
     }
