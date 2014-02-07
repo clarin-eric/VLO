f7feb538683ab8bf143d91e6e352b918d2181509
Twan Goosen <t.goosen@gmail.com>
2014-02-07 09:40:43 +0000

    started wicket search page based on mockup

--- vlo-web-app/src/main/java/eu/clarin/cmdi/FacetedSearchPage.java
+++ vlo-web-app/src/main/java/eu/clarin/cmdi/FacetedSearchPage.java
@@ -4,15 +4,11 @@ import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
 
-public class HomePage extends WebPage {
+public class FacetedSearchPage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
-	public HomePage(final PageParameters parameters) {
+	public FacetedSearchPage(final PageParameters parameters) {
 		super(parameters);
-
-		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
-
-		// TODO Add your page's components here
 
     }
 }
