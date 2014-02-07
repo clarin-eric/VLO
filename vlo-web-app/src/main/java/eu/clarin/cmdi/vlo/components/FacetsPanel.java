6f09a5c064208091680a14722dfc92a1176aa100
Twan Goosen <t.goosen@gmail.com>
2014-02-07 15:17:33 +0000

    Added wicket panels for facets sidebar item and individual facets. Filled with dummy data for now

---
+++ vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/components/FacetsPanel.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2014 CLARIN
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package eu.clarin.cmdi.vlo.components;
+
+import eu.clarin.cmdi.vlo.pojo.FacetValue;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.util.ListModel;
+
+/**
+ *
+ * @author twagoo
+ */
+public class FacetsPanel extends Panel {
+
+    public FacetsPanel(String id, ListModel<FacetValue> model) {
+        super(id, model);
+        add(new ListView<FacetValue>("facets", model) {
+
+            @Override
+            protected void populateItem(ListItem<FacetValue> item) {
+                //TODO: Check whether a value has been selected or not
+                item.add(new FacetPanel("facet", item.getModel()));
+            }
+        });
+    }
+
+}
--- vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/pages/FacetedSearchPage.java
+++ vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/pages/FacetedSearchPage.java
@@ -1,14 +1,23 @@
 package eu.clarin.cmdi.vlo.pages;
 
+import eu.clarin.cmdi.vlo.components.FacetsPanel;
+import eu.clarin.cmdi.vlo.pojo.Facet;
+import eu.clarin.cmdi.vlo.pojo.FacetValue;
+import java.util.Arrays;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.model.util.ListModel;
 
 public class FacetedSearchPage extends WebPage {
-	private static final long serialVersionUID = 1L;
 
-	public FacetedSearchPage(final PageParameters parameters) {
-		super(parameters);
+    private static final long serialVersionUID = 1L;
 
+    public FacetedSearchPage(final PageParameters parameters) {
+        super(parameters);
+
+        add(new FacetsPanel("facets", new ListModel<FacetValue>(Arrays.asList(
+                new FacetValue(new Facet("Language"), "Dutch"),
+                new FacetValue(new Facet("Resource type"), "Text")
+        ))));
     }
 }
