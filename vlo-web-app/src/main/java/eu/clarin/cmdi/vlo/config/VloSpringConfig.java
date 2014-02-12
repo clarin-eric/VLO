4a5f21e7fe373235bbcb7e15cb141b7716e99a6a
Twan Goosen <t.goosen@gmail.com>
2014-02-12 08:56:30 +0000

    Added Spring to the VLO web app

---
+++ vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/config/VloSpringConfig.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2014 CLARIN
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package eu.clarin.cmdi.vlo.config;
+
+import eu.clarin.cmdi.vlo.VloWicketApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Annotation based Spring configuration for the VLO web application.
+ *
+ * Note: this works because {@link org.apache.wicket.spring.SpringWebApplicationFactory}
+ * is used in place of the standard Wicket application factory and annotation 
+ * driven configuration is enabled in WEB-INF/applicationContext.xml
+ *
+ * @author twagoo
+ */
+@Configuration
+public class VloSpringConfig {
+
+    /**
+     *
+     * @return the web application object that represents the Wicket application
+     */
+    @Bean
+    VloWicketApplication webApplication() {
+        return new VloWicketApplication();
+    }
+}
