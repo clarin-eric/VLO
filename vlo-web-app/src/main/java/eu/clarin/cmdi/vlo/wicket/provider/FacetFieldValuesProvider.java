206781a49128f23cc8a9b5edd826f6fcbd49fbdb
twagoo <t.goosen@gmail.com>
2014-03-10 13:41:24 +0000

    Added support for sorting to field values provider

--- vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/wicket/provider/FacetFieldValuesProvider.java
+++ vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/wicket/provider/FacetFieldValuesProvider.java
@@ -16,20 +16,25 @@
  */
 package eu.clarin.cmdi.vlo.wicket.provider;
 
+import com.google.common.collect.Ordering;
+import eu.clarin.cmdi.vlo.pojo.FieldValuesOrder;
 import java.util.Iterator;
 import java.util.List;
 import org.apache.solr.client.solrj.response.FacetField;
-import org.apache.wicket.markup.repeater.data.IDataProvider;
+import org.apache.solr.client.solrj.response.FacetField.Count;
+import org.apache.wicket.extensions.markup.html.repeater.util.SortParam;
+import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 
 /**
  * Provides facet values and counts (both through the {@link Count} object of
- * SOLR) present on a {@link FacetField}
+ * SOLR) present on a {@link FacetField}, sortable by either count or name
  *
+ * @see FieldValuesOrder
  * @author twagoo
  */
-public class FacetFieldValuesProvider implements IDataProvider<FacetField.Count> {
+public class FacetFieldValuesProvider extends SortableDataProvider<FacetField.Count, FieldValuesOrder> {
 
     private final IModel<FacetField> model;
     private final int maxNumberOfItems;
@@ -44,15 +49,21 @@ public class FacetFieldValuesProvider implements IDataProvider<FacetField.Count>
         this(model, Integer.MAX_VALUE);
     }
 
+    public FacetFieldValuesProvider(IModel<FacetField> model, int max) {
+        this(model, max, new SortParam<FieldValuesOrder>(FieldValuesOrder.COUNT, false));
+    }
+
     /**
      * Creates a provider with a set maximum number of values
      *
      * @param model FacetField model to get values and counts for
      * @param max maximum number of values to show
+     * @param sort initial sort property and order
      */
-    public FacetFieldValuesProvider(IModel<FacetField> model, int max) {
+    public FacetFieldValuesProvider(IModel<FacetField> model, int max, SortParam<FieldValuesOrder> sort) {
         this.model = model;
         this.maxNumberOfItems = max;
+        setSort(sort);
     }
 
     @Override
@@ -60,7 +71,7 @@ public class FacetFieldValuesProvider implements IDataProvider<FacetField.Count>
         // TODO: From old VLO:
         // IGNORABLE_VALUES (like "unknown") are move to the back of the list and should only be shown when you click "more...", unless the list is too small then whe can just show them.
         final List<FacetField.Count> values = model.getObject().getValues();
-        return values.listIterator((int) first);
+        return getOrdering().sortedCopy(values).listIterator((int) first);
     }
 
     @Override
@@ -78,5 +89,38 @@ public class FacetFieldValuesProvider implements IDataProvider<FacetField.Count>
     public void detach() {
         model.detach();
     }
+
+    private Ordering getOrdering() {
+        final Ordering ordering;
+        if (getSort().getProperty() == FieldValuesOrder.COUNT) {
+            ordering = COUNT_ORDERING;
+        } else if (getSort().getProperty() == FieldValuesOrder.NAME) {
+            ordering = NAME_ORDERING;
+        } else {
+            ordering = Ordering.natural();
+        }
+
+        if (getSort().isAscending()) {
+            return ordering;
+        } else {
+            return ordering.reverse();
+        }
+    }
+
+    private static Ordering<FacetField.Count> COUNT_ORDERING = new Ordering<FacetField.Count>() {
+
+        @Override
+        public int compare(Count arg0, Count arg1) {
+            return Long.compare(arg0.getCount(), arg1.getCount());
+        }
+    };
+
+    private static Ordering<FacetField.Count> NAME_ORDERING = new Ordering<FacetField.Count>() {
+
+        @Override
+        public int compare(Count arg0, Count arg1) {
+            return arg0.getName().compareTo(arg1.getName());
+        }
+    };
 
 }
