2e496738cc6cb558eb19e493eba9fda1414ecd97
twagoo <t.goosen@gmail.com>
2014-03-21 13:11:22 +0000

    added constant for the handle proxy base url

--- vlo-commons/src/main/java/eu/clarin/cmdi/vlo/FacetConstants.java
+++ vlo-commons/src/main/java/eu/clarin/cmdi/vlo/FacetConstants.java
@@ -27,19 +27,23 @@ public class FacetConstants {
     public static final String FIELD_COMPLETE_METADATA = "metadataSource";
     public static final String FIELD_NATIONAL_PROJECT = "nationalProject";
     public static final String FIELD_KEYWORDS = "keywords";
-    
+
     //The _ facets are not meant to be shown to users.
     public static final String FIELD_FILENAME = "_fileName";
     public static final String FIELD_RESOURCE = "_resourceRef";
     public static final String FIELD_CLARIN_PROFILE = "_componentProfile";
     public static final String FIELD_SEARCH_SERVICE = "_contentSearchRef";
     public static final String FIELD_LAST_SEEN = "_lastSeen";
-    
-    /** Facet constant associated with the landing page type. */
+
+    /**
+     * Facet constant associated with the landing page type.
+     */
     public static final String FIELD_LANDINGPAGE = "_landingPageRef";
-    /** Facet constant associated with the search page type. */
+    /**
+     * Facet constant associated with the search page type.
+     */
     public static final String FIELD_SEARCHPAGE = "_searchPageRef";
-    
+
     //Normalized mimeTypes
     public static final String RESOURCE_TYPE_AUDIO = "audio";
     public static final String RESOURCE_TYPE_VIDEO = "video";
@@ -47,6 +51,11 @@ public class FacetConstants {
     public static final String RESOURCE_TYPE_IMAGE = "image";
     public static final String RESOURCE_TYPE_ANNOTATION = "annotation";
 
+    /**
+     * Handle proxy base url (to replace part that matches
+     * {@link #HANDLE_PREFIX})
+     */
+    public static final String HANDLE_PROXY = "http://hdl.handle.net/";
     public static final String HANDLE_MPI_PREFIX = "hdl:1839";
     public static final String HANDLE_PREFIX = "hdl:";
     public static final String TEST_HANDLE_MPI_PREFIX = "test-hdl:1839";
