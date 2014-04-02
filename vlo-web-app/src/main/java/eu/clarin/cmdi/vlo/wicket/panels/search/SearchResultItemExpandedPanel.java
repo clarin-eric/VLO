9c048a5406cca94b3bb40dd8e9a2e4348e10321e
twagoo <t.goosen@gmail.com>
2014-04-02 13:05:57 +0000

    added resources to expanded search result item view

--- vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/wicket/panels/search/SearchResultItemExpandedPanel.java
+++ vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/wicket/panels/search/SearchResultItemExpandedPanel.java
@@ -19,13 +19,26 @@ package eu.clarin.cmdi.vlo.wicket.panels.search;
 import eu.clarin.cmdi.vlo.FacetConstants;
 import eu.clarin.cmdi.vlo.pojo.SearchContext;
 import eu.clarin.cmdi.vlo.service.FieldFilter;
+import eu.clarin.cmdi.vlo.service.ResourceStringConverter;
+import eu.clarin.cmdi.vlo.wicket.ResourceTypeCssBehaviour;
 import eu.clarin.cmdi.vlo.wicket.components.RecordPageLink;
 import eu.clarin.cmdi.vlo.wicket.components.SolrFieldLabel;
+import eu.clarin.cmdi.vlo.wicket.model.CollectionListModel;
+import eu.clarin.cmdi.vlo.wicket.model.ResourceInfoModel;
+import eu.clarin.cmdi.vlo.wicket.model.SolrFieldModel;
 import eu.clarin.cmdi.vlo.wicket.panels.record.FieldsTablePanel;
 import eu.clarin.cmdi.vlo.wicket.provider.DocumentFieldsProvider;
+import java.util.List;
 import org.apache.solr.common.SolrDocument;
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.ExternalLink;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.GenericPanel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 /**
@@ -36,26 +49,58 @@ public class SearchResultItemExpandedPanel extends GenericPanel<SolrDocument> {
 
     @SpringBean(name = "basicPropertiesFilter") //TODO: make separate filter for properties in search result items
     private FieldFilter basicPropertiesFilter;
-    private final IModel<SearchContext> selectionModel;
+    @SpringBean
+    ResourceStringConverter resourceStringConverter;
 
     public SearchResultItemExpandedPanel(String id, IModel<SolrDocument> documentModel, IModel<SearchContext> selectionModel) {
         super(id, documentModel);
-        this.selectionModel = selectionModel;
 
         // add untruncated description
         add(new SolrFieldLabel("description", documentModel, FacetConstants.FIELD_DESCRIPTION, "<no description>"));
         add(new RecordPageLink("recordLink", documentModel, selectionModel));
 
+        // table with some basic properties
         add(new FieldsTablePanel("documentProperties", new DocumentFieldsProvider(documentModel, basicPropertiesFilter)));
-        
-        //TODO: Add resource list
+
+        createResourcesList("resource");
+    }
+
+    private void createResourcesList(String id) {
+        // list of resources in this record
+        // TODO: limit number of resources shown here?
+        final SolrFieldModel<String> resourceModel = new SolrFieldModel<String>(getModel(), FacetConstants.FIELD_RESOURCE);
+        final IModel<List<String>> resourceListModel = new CollectionListModel<String>(resourceModel);
+        add(new ListView<String>(id, resourceListModel) {
+
+            @Override
+            protected void populateItem(ListItem<String> item) {
+                // get resource string converted into a ResourceInfo model
+                final ResourceInfoModel resourceInfoModel = new ResourceInfoModel(resourceStringConverter, item.getModel());
+
+                // add a link to the resource with the file name as its label
+                final ExternalLink resourceLink = new ExternalLink("resourceLink", new PropertyModel(resourceInfoModel, "href"));
+                resourceLink.add(new Label("resourceName", new PropertyModel(resourceInfoModel, "fileName")));
+
+                // add a tooltip showing resource type and mime type
+                final StringResourceModel tooltipModel
+                        = new StringResourceModel("resource.tooltip", SearchResultItemExpandedPanel.this, resourceModel,
+                                new Object[]{
+                                    new PropertyModel(resourceInfoModel, "resourceType"),
+                                    new PropertyModel(resourceInfoModel, "mimeType")});
+                resourceLink.add(new AttributeAppender("title", tooltipModel));
+
+                // sets the css class depending on the resource type
+                item.add(new ResourceTypeCssBehaviour(resourceInfoModel));
+
+                // add to list
+                item.add(resourceLink);
+            }
+        });
     }
 
     @Override
     public void detachModels() {
         super.detachModels();
-        // not passed to super
-        selectionModel.detach();
     }
 
 }
