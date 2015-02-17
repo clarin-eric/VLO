930664e3dbd8a2a4837efb2f04ae05c04f165c53
teckart <teckart@informatik.uni-leipzig.de>
2015-02-17 10:51:52 +0000

    Extended post processor to deal with purl.org-URLs

--- vlo-importer/src/main/java/eu/clarin/cmdi/vlo/importer/ResourceClassPostProcessor.java
+++ vlo-importer/src/main/java/eu/clarin/cmdi/vlo/importer/ResourceClassPostProcessor.java
@@ -7,11 +7,16 @@ public class ResourceClassPostProcessor extends LanguageCodePostProcessor {
     /**
      * Postprocess ResourceClass values
      * @param value extracted ResourcClass information
-     * @return Value with upper case first letter
+     * @return Value with upper case first letter and some value normalisation
      */
     @Override
     public List<String> process(String value) {
-        String result =value;
+        String result = value;
+        
+        // replace DCMI URLs with DCMI type
+        result = result.replaceFirst("http://purl.org/dc/dcmitype/", "");
+        
+        // first letter should be upper case
         if(result.length() > 1) {
             result = result.substring(0, 1).toUpperCase().concat(result.substring(1, result.length()));
         }
