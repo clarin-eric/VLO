3c450c833a832a5776bc24638306a1a00f4ec338
twagoo <t.goosen@gmail.com>
2015-02-23 11:01:21 +0000

    Converted field value shown in breadcrumbs panel Refs #553

--- vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/wicket/panels/BreadCrumbPanel.java
+++ vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/wicket/panels/BreadCrumbPanel.java
@@ -23,6 +23,7 @@ import eu.clarin.cmdi.vlo.service.PageParametersConverter;
 import eu.clarin.cmdi.vlo.wicket.model.SolrFieldNameModel;
 import eu.clarin.cmdi.vlo.wicket.pages.FacetedSearchPage;
 import eu.clarin.cmdi.vlo.wicket.provider.FacetSelectionProvider;
+import eu.clarin.cmdi.vlo.wicket.provider.FieldValueConverterProvider;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Locale;
@@ -51,24 +52,26 @@ import org.apache.wicket.util.convert.IConverter;
  * @author twagoo
  */
 public class BreadCrumbPanel extends GenericPanel<QueryFacetsSelection> {
-
+    
     @SpringBean(name = "queryParametersConverter")
     private PageParametersConverter<QueryFacetsSelection> paramsConverter;
-
+    @SpringBean
+    private FieldValueConverterProvider fieldValueConverterProvider;
+    
     private final WebMarkupContainer query;
     private final WebMarkupContainer facets;
-
+    
     public BreadCrumbPanel(String id, IModel<QueryFacetsSelection> model) {
         super(id, model);
         add(new BookmarkablePageLink("searchPage", FacetedSearchPage.class));
         add(query = createQuery(model, "query"));
         add(facets = createFacets(model, "facets"));
     }
-
+    
     private WebMarkupContainer createQuery(final IModel<QueryFacetsSelection> selectionModel, String id) {
         final WebMarkupContainer queryContainer = new WebMarkupContainer(id);
         final Link link = new AjaxFallbackLink("leavequery") {
-
+            
             @Override
             public void onClick(AjaxRequestTarget target) {
                 // make query object without selection
@@ -80,11 +83,11 @@ public class BreadCrumbPanel extends GenericPanel<QueryFacetsSelection> {
         queryContainer.add(link);
         return queryContainer;
     }
-
+    
     private WebMarkupContainer createFacets(final IModel<QueryFacetsSelection> model, String id) {
         final WebMarkupContainer facetsContainer = new WebMarkupContainer(id);
         facetsContainer.add(new AjaxFallbackLink("leaveselection") {
-
+            
             @Override
             public void onClick(AjaxRequestTarget target) {
                 onSelectionChanged(model.getObject(), target);
@@ -94,19 +97,20 @@ public class BreadCrumbPanel extends GenericPanel<QueryFacetsSelection> {
         // create a provider that lists the facet name -> values entries
         final FacetSelectionProvider facetSelectionProvider = new FacetSelectionProvider(model);
         facetsContainer.add(new DataView<Map.Entry<String, FacetSelection>>("facet", facetSelectionProvider) {
-
+            
             @Override
             protected void populateItem(final Item<Map.Entry<String, FacetSelection>> item) {
                 final IModel<Map.Entry<String, FacetSelection>> selectionModel = item.getModel();
                 // add a label for the selected facet value(s)
                 final Label valueLabel = new Label("value", new PropertyModel(selectionModel, "value")) {
-
+                    
                     @Override
                     public <C> IConverter<C> getConverter(Class<C> type) {
+                        final String facet = item.getModelObject().getKey();
                         // converter to render the value(s) nicely
-                        return (IConverter<C>) new SelectionConverter(item.getModelObject().getKey());
+                        return (IConverter<C>) new SelectionConverter(facet, fieldValueConverterProvider.getConverter(facet));
                     }
-
+                    
                 };
                 // add facet name as title attribute so that it becomes available through a tooltip
                 valueLabel.add(new AttributeModifier("title",
@@ -115,7 +119,7 @@ public class BreadCrumbPanel extends GenericPanel<QueryFacetsSelection> {
 
                 // add a link for removal of the facet value selection
                 item.add(new AjaxFallbackLink("removal") {
-
+                    
                     @Override
                     public void onClick(AjaxRequestTarget target) {
                         // get a copy of the current selection
@@ -128,7 +132,7 @@ public class BreadCrumbPanel extends GenericPanel<QueryFacetsSelection> {
                 });
             }
         });
-
+        
         return facetsContainer;
     }
 
@@ -143,14 +147,14 @@ public class BreadCrumbPanel extends GenericPanel<QueryFacetsSelection> {
     protected void onSelectionChanged(QueryFacetsSelection selection, AjaxRequestTarget target) {
         setResponsePage(FacetedSearchPage.class, paramsConverter.toParameters(selection));
     }
-
+    
     @Override
     protected void onConfigure() {
         super.onConfigure();
-
+        
         final String queryString = getModelObject().getQuery();
         final Map<String, FacetSelection> selection = getModelObject().getSelection();
-
+        
         query.setVisible(queryString != null && !queryString.isEmpty());
         facets.setVisible(selection != null && !selection.isEmpty());
     }
@@ -160,58 +164,70 @@ public class BreadCrumbPanel extends GenericPanel<QueryFacetsSelection> {
      * collection (if singleton, show its value; if multiple, comma separated)
      */
     private class SelectionConverter implements IConverter<FacetSelection> {
-
+        
         private final String facet;
-
-        public SelectionConverter(String facet) {
+        private final IConverter<String> valueConverter;
+        
+        public SelectionConverter(String facet, IConverter<String> valueConverter) {
             this.facet = facet;
+            this.valueConverter = valueConverter;
         }
-
+        
         @Override
         public FacetSelection convertToObject(String value, Locale locale) throws ConversionException {
             throw new UnsupportedOperationException("Not supported yet.");
         }
-
+        
         @Override
         public String convertToString(FacetSelection selection, Locale locale) {
             switch (selection.getSelectionType()) {
                 case AND:
-                    return getCollectionString(selection, " and ");
+                    return getCollectionString(selection, " and ", locale);
                 case OR:
-                    return getCollectionString(selection, " or ");
+                    return getCollectionString(selection, " or ", locale);
                 case NOT:
-                    return "not [" + getCollectionString(selection, " or ") + "]";
+                    return "not [" + getCollectionString(selection, " or ", locale) + "]";
                 case NOT_EMPTY:
                     return getAnyValueString();
                 default:
                     return facet;
             }
-
+            
         }
-
+        
         private String getAnyValueString() {
             if (FacetConstants.FIELD_SEARCH_SERVICE.equals(facet)) {
                 return "Content searchable";
             }
             return "any " + facet;
         }
-
-        public String getCollectionString(FacetSelection selection, String valueSeparator) {
+        
+        public String getCollectionString(FacetSelection selection, String valueSeparator, Locale locale) {
             final Collection<String> value = selection.getValues();
             //TODO: include selection type
             if (value.isEmpty()) {
                 return "";
             } else if (value.size() == 1) {
-                return value.iterator().next();
+                return getConvertedValue(value.iterator().next(), locale);
             } else {
                 final Iterator<String> iterator = value.iterator();
-                final StringBuilder sb = new StringBuilder(iterator.next());
+                final StringBuilder sb = new StringBuilder(getConvertedValue(iterator.next(), locale));
                 while (iterator.hasNext()) {
-                    sb.append(valueSeparator).append(iterator.next());
+                    sb.append(valueSeparator).append(getConvertedValue(iterator.next(), locale));
                 }
                 return sb.toString();
             }
         }
-
+        
+        private String getConvertedValue(String string, Locale locale) {
+            if (valueConverter != null) {
+                final String converted = valueConverter.convertToString(string, locale);
+                if (converted != null) {
+                    return converted;
+                }
+            }
+            return string;
+        }
+        
     };
 }
