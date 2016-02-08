e6a438668f3338bdfbe0abe3d4e79f4b2097bea9
Twan Goosen <twan.goosen@mpi.nl>
2016-02-08 14:42:21 +0100

    availability icons on record page

--- vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/wicket/panels/record/RecordLicenseInfoPanel.java
+++ vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/wicket/panels/record/RecordLicenseInfoPanel.java
@@ -17,16 +17,24 @@
 package eu.clarin.cmdi.vlo.wicket.panels.record;
 
 import eu.clarin.cmdi.vlo.FacetConstants;
+import eu.clarin.cmdi.vlo.wicket.model.CollectionListModel;
+import eu.clarin.cmdi.vlo.wicket.model.ConvertedFieldValueModel;
+import eu.clarin.cmdi.vlo.wicket.model.FormattedStringModel;
 import eu.clarin.cmdi.vlo.wicket.model.HandleLinkModel;
 import eu.clarin.cmdi.vlo.wicket.model.SolrFieldModel;
 import eu.clarin.cmdi.vlo.wicket.model.SolrFieldStringModel;
 import java.util.Collection;
 import org.apache.solr.common.SolrDocument;
 import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.ExternalLink;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.GenericPanel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 
 /**
  *
@@ -58,7 +66,34 @@ public class RecordLicenseInfoPanel extends GenericPanel<SolrDocument> {
             }
 
         };
+
+        //add 'tags' for all availability values
+        container.add(new ListView<String>("availability", new CollectionListModel<>(availabilityModel)) {
+            @Override
+            protected void populateItem(ListItem<String> item) {
+                item.add(new Label("availabilityDescription",
+                        new FormattedStringModel(Model.of("Availability: %s"),
+                                new ConvertedFieldValueModel(item.getModel(), FacetConstants.FIELD_AVAILABILITY)))); //TODO: description instead
+                item.add(new AttributeAppender("class", item.getModel(), " "));
+            }
+        });
+
+//        //add 'tag' for all licence values
+//        final WebMarkupContainer licenseTag = new WebMarkupContainer("license") {
+//            @Override
+//            protected void onConfigure() {
+//                setVisible(licenseModel.getObject() != null);
+//            }
+//        };
+//
+//        //TODO: turn into link to licence section of the record page
+//        add(licenseTag
+//                .add(new AttributeAppender("class", licenseModel, " ")) //TODO: map to id for license image (via css class)
+//                .add(new AttributeModifier("title", licenseModel)) //TODO: map to license name
+//        );
+
         container.add(createOriginalContextContainer("originalContext"));
+        
         return container;
     }
 
@@ -78,7 +113,7 @@ public class RecordLicenseInfoPanel extends GenericPanel<SolrDocument> {
         final SolrFieldStringModel valueModel = new SolrFieldStringModel(getModel(), FacetConstants.FIELD_LANDINGPAGE);
         // wrap in model that transforms handle links
         final IModel<String> landingPageHrefModel = new HandleLinkModel(valueModel);
-        
+
         //create container
         final MarkupContainer originalContext = new WebMarkupContainer(id) {
 
@@ -87,14 +122,14 @@ public class RecordLicenseInfoPanel extends GenericPanel<SolrDocument> {
                 setVisible(landingPageHrefModel.getObject() != null);
             }
         };
-        
+
         // add landing page link
         originalContext.add(new ExternalLink("landingPage", landingPageHrefModel));
-        
+
         return originalContext;
     }
 
-    public boolean isInfoAvailable() {
+    private boolean isInfoAvailable() {
         final Collection<String> availability = availabilityModel.getObject();
         return accessInfoModel.getObject() != null || licenseModel.getObject() != null
                 || (availability != null && !availability.contains(AVAILABILITY_UNSPECIFIED));
