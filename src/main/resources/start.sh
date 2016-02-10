913cd5d3c0717eb8bba1532fde6ff26062a52ae1
davoros <ostojic.davor@gmail.com>
2016-02-10 11:37:39 +0100

    mvn conf for distribution

--- src/main/resources/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Get the script's source directory
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-JAVA=`which java 2>/dev/null`
-
-
-$JAVA -Xmx2G -jar "${DIR}/sitemap-gen-0.0.1-SNAPSHOT.jar"
