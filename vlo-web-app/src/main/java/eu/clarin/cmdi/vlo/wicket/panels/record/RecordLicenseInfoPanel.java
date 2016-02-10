66a8a13ed9d59ecffb3cdbed5e91a1cd373aa1e3
Twan Goosen <twan.goosen@mpi.nl>
2016-02-10 09:51:09 +0100

    Added licences to 'license info panel' of record page

--- vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/wicket/panels/record/RecordLicenseInfoPanel.java
+++ vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/wicket/panels/record/RecordLicenseInfoPanel.java
@@ -22,7 +22,6 @@ import eu.clarin.cmdi.vlo.config.FieldValueDescriptor;
 import eu.clarin.cmdi.vlo.config.VloConfig;
 import eu.clarin.cmdi.vlo.wicket.model.CollectionListModel;
 import eu.clarin.cmdi.vlo.wicket.model.ConvertedFieldValueModel;
-import eu.clarin.cmdi.vlo.wicket.model.FormattedStringModel;
 import eu.clarin.cmdi.vlo.wicket.model.HandleLinkModel;
 import eu.clarin.cmdi.vlo.wicket.model.MapValueModel;
 import eu.clarin.cmdi.vlo.wicket.model.NullFallbackModel;
@@ -31,7 +30,6 @@ import eu.clarin.cmdi.vlo.wicket.model.SolrFieldStringModel;
 import java.util.Collection;
 import java.util.Map;
 import org.apache.solr.common.SolrDocument;
-import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.html.WebMarkupContainer;
@@ -41,7 +39,6 @@ import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.GenericPanel;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.util.MapModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
@@ -59,13 +56,13 @@ public class RecordLicenseInfoPanel extends GenericPanel<SolrDocument> {
 
     private final IModel<Collection<String>> availabilityModel;
     private final IModel<Collection<String>> accessInfoModel;
-    private final IModel<String> licenseModel;
+    private final IModel<Collection<String>> licensesModel;
 
     public RecordLicenseInfoPanel(String id, IModel<SolrDocument> model) {
         super(id, model);
         this.availabilityModel = new SolrFieldModel<>(getModel(), FacetConstants.FIELD_AVAILABILITY);
         this.accessInfoModel = new SolrFieldModel<>(getModel(), FacetConstants.FIELD_ACCESS_INFO);
-        this.licenseModel = new SolrFieldStringModel(getModel(), FacetConstants.FIELD_LICENSE);
+        this.licensesModel = new SolrFieldModel<>(getModel(), FacetConstants.FIELD_LICENSE);
 
         add(createInfoContainer("availableInfo"));
         add(createNoInfoContainer("noInfo"));
@@ -80,12 +77,25 @@ public class RecordLicenseInfoPanel extends GenericPanel<SolrDocument> {
 
         };
 
+        //add labels for all license values
+        container.add(createLicenseItems("license"));
         //add labels for all availability values
         container.add(createAvailabilityItems("availability"));
 
         container.add(createOriginalContextContainer("originalContext"));
 
         return container;
+    }
+
+    private ListView<String> createLicenseItems(final String id) {
+        return new ListView<String>(id, new CollectionListModel<>(licensesModel)) {
+            @Override
+            protected void populateItem(ListItem<String> item) {
+                //TODO: add link to licence page
+                item.add(new Label("licenseName", new ConvertedFieldValueModel(item.getModel(), FacetConstants.FIELD_LICENSE)));
+                item.add(new AttributeAppender("class", item.getModel(), " "));
+            }
+        };
     }
 
     private ListView<String> createAvailabilityItems(final String id) {
@@ -112,19 +122,6 @@ public class RecordLicenseInfoPanel extends GenericPanel<SolrDocument> {
         };
     }
 
-//        //add 'tag' for all licence values
-//        final WebMarkupContainer licenseTag = new WebMarkupContainer("license") {
-//            @Override
-//            protected void onConfigure() {
-//                setVisible(licenseModel.getObject() != null);
-//            }
-//        };
-//
-//        //TODO: turn into link to licence section of the record page
-//        add(licenseTag
-//                .add(new AttributeAppender("class", licenseModel, " ")) //TODO: map to id for license image (via css class)
-//                .add(new AttributeModifier("title", licenseModel)) //TODO: map to license name
-//        );
     private WebMarkupContainer createNoInfoContainer(final String id) {
         final WebMarkupContainer container = new WebMarkupContainer(id) {
             @Override
@@ -159,7 +156,7 @@ public class RecordLicenseInfoPanel extends GenericPanel<SolrDocument> {
 
     private boolean isInfoAvailable() {
         final Collection<String> availability = availabilityModel.getObject();
-        return accessInfoModel.getObject() != null || licenseModel.getObject() != null
+        return accessInfoModel.getObject() != null || licensesModel.getObject() != null
                 || (availability != null && !availability.contains(AVAILABILITY_UNSPECIFIED));
     }
 
