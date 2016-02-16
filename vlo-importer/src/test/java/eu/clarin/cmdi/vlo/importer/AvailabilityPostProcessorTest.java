3be8049a1c24ebde7920804a64bf358e8d9d720c
Twan Goosen <twan.goosen@mpi.nl>
2016-02-16 09:33:38 +0100

    Updated availability post processor test now ';' separated values are processed properly

--- vlo-importer/src/test/java/eu/clarin/cmdi/vlo/importer/AvailabilityPostProcessorTest.java
+++ vlo-importer/src/test/java/eu/clarin/cmdi/vlo/importer/AvailabilityPostProcessorTest.java
@@ -10,12 +10,12 @@ public class AvailabilityPostProcessorTest extends ImporterTestcase {
     @Test
     public void testAvailabilityMapping() {
         PostProcessor processor = new AvailabilityPostProcessor();     
-        assertEquals("PUB;BY", processor.process("Apache Licence 2.0").get(0));
+        assertEquals("PUB", processor.process("Apache Licence 2.0").get(0));
         assertEquals("PUB", processor.process("Open Access").get(0));
         assertEquals("PUB", processor.process("open access").get(0));
         assertEquals("PUB", processor.process("Open Access").get(0));
         assertEquals("ACA", processor.process("free; Free for academic use.").get(0));
-        assertEquals("PUB;BY;NC", processor.process("CC BY-NC 3.0 DE").get(0));
+        assertEquals("PUB", processor.process("CC BY-NC 3.0 DE").get(0));
         assertEquals("RES", processor.process("Please contact contact-person").get(0));
         
         
