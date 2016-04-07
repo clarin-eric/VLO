08924966049801e8ff62108e3cf5a686d536ce88
Twan Goosen <twan.goosen@mpi.nl>
2016-04-07 14:56:26 +0200

    TODOs

--- vlo-statistics/src/main/java/clarin/cmdi/vlo/statistics/reporting/StatsdReporter.java
+++ vlo-statistics/src/main/java/clarin/cmdi/vlo/statistics/reporting/StatsdReporter.java
@@ -17,6 +17,8 @@
 package clarin.cmdi.vlo.statistics.reporting;
 
 import clarin.cmdi.vlo.statistics.model.VloReport;
+import com.timgroup.statsd.ConvenienceMethodProvidingStatsDClient;
+import com.timgroup.statsd.NoOpStatsDClient;
 import com.timgroup.statsd.NonBlockingStatsDClient;
 import com.timgroup.statsd.StatsDClient;
 import org.slf4j.Logger;
@@ -43,7 +45,7 @@ public class StatsdReporter implements VloReportHandler {
     @Override
     public void handleReport(VloReport report) {
         logger.info("Sending reports to statsd server {}:{} with prefix '{}'", statsdHost, statsdPort, statsdPrefix);
-        final StatsDClient client = new NonBlockingStatsDClient(statsdPrefix + ".index", statsdHost, statsdPort);
+        final ConvenienceMethodProvidingStatsDClient client = new NonBlockingStatsDClient(statsdPrefix + ".index", statsdHost, statsdPort);
 
         try {
             //send record count
@@ -54,11 +56,13 @@ public class StatsdReporter implements VloReportHandler {
                 final String name = counts.getCollection().replaceAll("[^A-z0-9]", "_");
                 client.gauge("collections." + name, counts.getCount());
             });
+            //TODO: get collections from previous report and set those that have disappeared to 0
 
             //send facet values
             report.getFacets().stream().forEach((facet) -> {
                 client.gauge("facetValueCounts." + facet.getName(), facet.getValueCount());
             });
+            //TODO: get facets from previous report and set those that have disappeared to 0
         } finally {
             client.stop();
         }
