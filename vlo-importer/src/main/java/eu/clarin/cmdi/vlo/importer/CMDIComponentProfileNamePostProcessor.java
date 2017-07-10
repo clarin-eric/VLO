ada01d819d000b7d95757c9068521e064c45b7a1
Twan Goosen <twan@clarin.eu>
2017-07-10 15:22:41 +0200

    Thread safe post processing for _componentProfile field

--- vlo-importer/src/main/java/eu/clarin/cmdi/vlo/importer/CMDIComponentProfileNamePostProcessor.java
+++ vlo-importer/src/main/java/eu/clarin/cmdi/vlo/importer/CMDIComponentProfileNamePostProcessor.java
@@ -1,17 +1,14 @@
 package eu.clarin.cmdi.vlo.importer;
 
 import com.ximpleware.AutoPilot;
-import com.ximpleware.NavException;
+import com.ximpleware.VTDException;
 import com.ximpleware.VTDGen;
 import com.ximpleware.VTDNav;
-import com.ximpleware.XPathEvalException;
-import com.ximpleware.XPathParseException;
 import eu.clarin.cmdi.vlo.config.VloConfig;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,9 +21,8 @@ public class CMDIComponentProfileNamePostProcessor extends AbstractPostProcessor
     private static final String XPATH = "/ComponentSpec/Header/Name/text()";
     private String BASE_URL = null;
 
-    private static final String EMPTY_STRING = "";
     private final static Logger LOG = LoggerFactory.getLogger(CMDIComponentProfileNamePostProcessor.class);
-    private final Map<String, String> cache = new ConcurrentHashMap<>();
+    private final ConcurrentMap<String, List<String>> cache = new ConcurrentHashMap<>();
 
     public CMDIComponentProfileNamePostProcessor(VloConfig config) {
         super(config);
@@ -34,49 +30,44 @@ public class CMDIComponentProfileNamePostProcessor extends AbstractPostProcessor
 
     @Override
     public List<String> process(String profileId, CMDIData cmdiData) {
-        String result = EMPTY_STRING;
         if (profileId != null) {
-            if (cache.containsKey(profileId)) {
-                result = cache.get(profileId);
-            } else {
-                VTDGen vg;
-                VTDNav vn;
-
-                final AutoPilot ap= new AutoPilot();
+            return cache.computeIfAbsent(profileId, (key) -> {
                 try {
-                    ap.selectXPath(XPATH);
-                } catch (XPathParseException e) {
-                    LOG.error(e.getLocalizedMessage());
-                    return Collections.singletonList(result);
+                    return Collections.singletonList(calculate(key));
+                } catch (VTDException ex) {
+                    LOG.error("Error while looking up profile name for profile {}", profileId, ex);
+                    return null;
                 }
-                vg = new VTDGen();
-                BASE_URL = getConfig().getComponentRegistryRESTURL();
-
-                LOG.debug("PARSING PROFILE: " + BASE_URL + profileId);
-                // get the name of the profile from the expanded xml in the component registry
-                if (vg.parseHttpUrl(BASE_URL + profileId + "/xml", true)) {
-                    LOG.debug("PARSED PROFILE: " + BASE_URL + profileId);
-                    vn = vg.getNav();
-                    ap.bind(vn);
-                    int idx;
-                    try {
-                        idx = ap.evalXPath();
-                        LOG.debug("EVALUATED XPATH: " + XPATH + " found idx: " + idx);
-                        if (idx == -1) { // idx represent the nodeId in the xml file, if -1 the xpath evaluates to nothing.
-                            Collections.singletonList(result);
-                        }
-                        result = vn.toString(idx);
-                        cache.put(profileId, result);
-                    } catch (NavException | XPathEvalException e) {
-                        LOG.error(e.getLocalizedMessage());
-                        Collections.singletonList(result);
-                    }
-                } else {
-                    LOG.error("Cannot open and/or parse XML Schema: {}.", BASE_URL + profileId);
-                }
-            }
+            });
+        } else {
+            return Collections.singletonList("");
         }
-        return Collections.singletonList(result);
+    }
+
+    private String calculate(String profileId) throws VTDException {
+        final AutoPilot ap = new AutoPilot();
+        ap.selectXPath(XPATH);
+        final VTDGen vg = new VTDGen();
+        BASE_URL = getConfig().getComponentRegistryRESTURL();
+
+        LOG.debug("PARSING PROFILE: " + BASE_URL + profileId);
+        // get the name of the profile from the expanded xml in the component registry
+        if (vg.parseHttpUrl(BASE_URL + profileId + "/xml", true)) {
+            LOG.debug("PARSED PROFILE: " + BASE_URL + profileId);
+            final VTDNav vn = vg.getNav();
+            ap.bind(vn);
+            int idx;
+            idx = ap.evalXPath();
+            LOG.debug("EVALUATED XPATH: " + XPATH + " found idx: " + idx);
+            if (idx == -1) { // idx represent the nodeId in the xml file, if -1 the xpath evaluates to nothing.
+                LOG.warn("No profile name in definition for {}", profileId);
+                return "";
+            }
+            return vn.toString(idx);
+        } else {
+            LOG.error("Cannot open and/or parse XML Schema: {}.", BASE_URL + profileId);
+            throw new VTDException("Cannot open and/or parse XML Schema");
+        }
     }
 
     @Override
