b80a073f0dbb65fb47e0b3dc26df8b5bd1098b7c
Twan Goosen <twan@clarin.eu>
2017-10-25 11:03:48 +0200

    Creation of Solr collection in unpacked Solr instance

--- vlo-solr/build.sh
+++ vlo-solr/build.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 SOLR_DIST_URL="http://ftp.tudelft.nl/apache/lucene/solr/6.6.2/solr-6.6.2.tgz"
+SOLR_TARGET_DIR="target/solr"
 
 set -e
 
-if [ -d "target/solr" ]
+if [ -d "${SOLR_TARGET_DIR}" ]
 then
 	echo "Solr target directory already exists"
 	exit 1
 else
-	mkdir -p "target/solr"
+	mkdir -p "${SOLR_TARGET_DIR}"
 fi
 
 (
-	cd "target/solr"
-	echo Downloading and extracting Solr...
+	cd "${SOLR_TARGET_DIR}"
+	echo "====\nDownloading and extracting Solr...\n====\n"
 	curl -\# -L "${SOLR_DIST_URL}" | tar zxf - --strip-components 1
 
 	#create "collection1"
-	echo Creating Solr collection... 
+	echo "\n====\nCreating Solr collection... \n====\n"
 	bin/solr start
 	bin/solr create -c "collection1"
 	bin/solr stop
@@ -25,3 +26,6 @@ fi
 	#TODO: copy in config
 )
 
+echo "Completed building pre-configured VLO Solr instance in ${SOLR_TARGET_DIR}
+
+You can start it by running '${SOLR_TARGET_DIR}/bin/solr start'"
