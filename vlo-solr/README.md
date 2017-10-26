bd67e3cf41c76cd7dec99a6642abd30bea22c9a6
Twan Goosen <twan@clarin.eu>
2017-10-26 16:01:38 +0200

    Prepare separate solr home directory

--- vlo-solr/README.md
+++ vlo-solr/README.md
@@ -17,15 +17,16 @@ a distribution package `vlo-solr.tar.gz` than you can deploy on a server.
 There are various ways of running the Solr instance. For instructions on how to best 
 deploy the Solr instance, see the following page:
 
-	https://lucene.apache.org/solr/guide/6_6/taking-solr-to-production.html#taking-solr-to-production
+	https://lucene.apache.org/solr/guide/7_0/taking-solr-to-production.html#taking-solr-to-production
 
 The output of this build script can also be used in the build process of a dedicated 
 Docker image.
 
-Make sure to configure the Solr data directory via the `solr.data.dir` system property.
+Make sure to configure the Solr home directory via the `solr.solr.home` system property
+and the Solr data directory via the `solr.data.dir` property.
 For example, if starting the Solr instance using the "solr start" command, do as follows:
 
-	${SOLR_BIN_DIR}/solr start -Dsolr.data.dir=/var/solr-data
+	${SOLR_BIN_DIR}/solr start -s /srv/vlo/config/solr -t /var/solr-data
 
 Or add the setting of the property to `SOLR_OPTS` variable in the `solr.in.sh` file (see 
 the official Solr documentation instructions for details).
--- vlo-solr/build-solr.sh
+++ vlo-solr/build-solr.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 SOLR_VERSION="7.1.0"
-SOLR_DIST_URL="http://ftp.tudelft.nl/apache/lucene/solr/${SOLR_VERSION}/solr-${SOLR_VERSION}.tgz"
+#SOLR_DIST_URL="http://ftp.tudelft.nl/apache/lucene/solr/${SOLR_VERSION}/solr-${SOLR_VERSION}.tgz"
+SOLR_DIST_URL="file:/Users/twagoo/Desktop/solr-${SOLR_VERSION}.tgz"
 SOLR_TARGET_DIR="target/solr"
+SOLR_HOME_TARGET_DIR="target/solr-home-vlo"
 SOLR_TARGET_DIST="target/vlo-solr.tar.gz"
 SOLR_COLLECTION_NAME="collection1"
 SOLR_CONFIGURATION_SOURCE_DIR="`pwd`/../vlo-web-app/src/test/resources/solr/collection1"
@@ -11,10 +13,10 @@ set -e
 if [ "clean" = "$1" ]
 then
 	# Clean build output
-	if [ -d "${SOLR_TARGET_DIR}" ]
+	if [ -d "${SOLR_TARGET_DIR}" ] || [ -d "${SOLR_TARGET_DIST}" ] || [ -d "${SOLR_HOME_TARGET_DIR}" ]
 	then
-		echo "Removing ${SOLR_TARGET_DIR} and ${SOLR_TARGET_DIST}"
-		rm -rf "${SOLR_TARGET_DIR}" "${SOLR_TARGET_DIST}"
+		echo "Removing ${SOLR_TARGET_DIR}, ${SOLR_TARGET_DIST} and ${SOLR_HOME_TARGET_DIR}"
+		rm -rf "${SOLR_TARGET_DIR}" "${SOLR_TARGET_DIST}" "${SOLR_HOME_TARGET_DIR}"
 	else
 		echo "Nothing to do"
 	fi
@@ -37,6 +39,13 @@ else
 	mkdir -p "${SOLR_TARGET_DIR}"
 fi
 
+# Make a build output directory, or fail if it already exists
+if [ -d "${SOLR_HOME_TARGET_DIR}" ]
+then
+	echo "Solr home target directory already exists. Run '$0 clean' to clean up."
+	exit 1
+fi
+
 (
 	cd "${SOLR_TARGET_DIR}"
 	echo "====\nDownloading and extracting Solr...\n====\n"
@@ -45,28 +54,43 @@ fi
 	#create collection
 	echo "\n====\nCreating Solr collection '${SOLR_COLLECTION_NAME}'... \n====\n"
 	bin/solr start -p 8989
-	bin/solr create -c "${SOLR_COLLECTION_NAME}"
+	bin/solr create -c "${SOLR_COLLECTION_NAME}" -p 8989
 	bin/solr stop -p 8989
+)
 
-	echo "\n====\nApply VLO Solr configuration... \n====\n"
-	(
-		cd "server/solr/${SOLR_COLLECTION_NAME}"
-		#remove default configuration
-		rm -f \
-			conf/managed-schema \
-			conf/solrconfig.xml \
-			conf/*.txt
-		#copy in custom configuration
-		cp -r ${SOLR_CONFIGURATION_SOURCE_DIR}/* .
-	)	
+# Make solr home and apply configuration	
+echo cp -r "${SOLR_TARGET_DIR}/server/solr" "${SOLR_HOME_TARGET_DIR}"
+cp -r "${SOLR_TARGET_DIR}/server/solr" "${SOLR_HOME_TARGET_DIR}"
+
+echo "\n====\nApply VLO Solr configuration... \n====\n"
+(
+	cd "${SOLR_HOME_TARGET_DIR}/${SOLR_COLLECTION_NAME}"
+	#remove default configuration
+	rm -f \
+		conf/managed-schema \
+		conf/solrconfig.xml \
+		conf/*.txt
+	#copy in custom configuration
+	cp -r ${SOLR_CONFIGURATION_SOURCE_DIR}/* .
+)
+
+# Clean up server
+(
+	cd "${SOLR_TARGET_DIR}"
+	#delete collection
+	echo "\n====\nDeleting Solr collection '${SOLR_COLLECTION_NAME}' from server configuration... \n====\n"
+	bin/solr start -p 8989
+	bin/solr delete -c "${SOLR_COLLECTION_NAME}" -p 8989
+	bin/solr stop -p 8989
 )
 
 # Make tarball
 echo "\n====\nPackaging VLO Solr instance... \n====\n"
-(cd target && tar zc solr) > ${SOLR_TARGET_DIST}
+(cd target && tar zc solr solr-home-vlo) > ${SOLR_TARGET_DIST}
 
-echo "Completed building pre-configured VLO Solr instance in ${SOLR_TARGET_DIR}
+echo "Completed building VLO Solr instance in ${SOLR_TARGET_DIR} with configured
+Solr home in ${SOLR_HOME_TARGET_DIR}
 
 Created '${SOLR_TARGET_DIST}' for distribution
 
-You can also start the server by running '${SOLR_TARGET_DIR}/bin/solr start'"
+You can also start the server by running '${SOLR_TARGET_DIR}/bin/solr start -s ${SOLR_HOME_TARGET_DIR} [-t /my/solr/data/directory]'"
