5728877f48de306472fcc1a8a0687cd3bc45520a
Twan Goosen <twan@clarin.eu>
2017-11-02 11:07:02 +0100

    Improvements to the solr "build" script

--- vlo-solr/build-solr.sh
+++ vlo-solr/build-solr.sh
@@ -1,25 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 SOLR_VERSION="7.1.0"
-SOLR_DIST_URL="http://ftp.tudelft.nl/apache/lucene/solr/${SOLR_VERSION}/solr-${SOLR_VERSION}.tgz"
-SOLR_DIST_SIGNATURE_URL="http://www-eu.apache.org/dist/lucene/solr/7.1.0/solr-7.1.0.tgz.asc"
-SOLR_DIST_KEYS_URL="http://www-eu.apache.org/dist/lucene/solr/7.1.0/KEYS"
+SOLR_DIST_FILE="solr-${SOLR_VERSION}.tgz"
+SOLR_DIST_URL="http://ftp.tudelft.nl/apache/lucene/solr/${SOLR_VERSION}/${SOLR_DIST_FILE}"
+SOLR_DIST_SIGNATURE_URL="http://www-eu.apache.org/dist/lucene/solr/${SOLR_VERSION}/${SOLR_DIST_FILE}.asc"
+SOLR_DIST_KEYS_URL="http://www-eu.apache.org/dist/lucene/solr/${SOLR_VERSION}/KEYS"
 
 SOLR_TARGET_DIR="target/solr"
 
 SOLR_HOME_TEMPLATE_DIR="`pwd`/solr-home"
 SOLR_HOME_TARGET_DIR="target/solr-home-vlo"
 
-SOLR_TARGET_DIST="target/vlo-solr.tar.gz"
-
 set -e
 
 if [ "clean" = "$1" ]
 then
 	# Clean build output
-	if [ -d "${SOLR_TARGET_DIR}" ] || [ -d "${SOLR_TARGET_DIST}" ] || [ -d "${SOLR_HOME_TARGET_DIR}" ]
+	if [ -d "${SOLR_TARGET_DIR}" ] || [ -d "${SOLR_HOME_TARGET_DIR}" ] || [ -e "target/${SOLR_DIST_FILE}" ]
 	then
-		echo "Removing ${SOLR_TARGET_DIR}, ${SOLR_TARGET_DIST} and ${SOLR_HOME_TARGET_DIR}"
-		rm -rf "${SOLR_TARGET_DIR}" "${SOLR_TARGET_DIST}" "${SOLR_HOME_TARGET_DIR}"
+		echo "Removing ${SOLR_TARGET_DIR}, ${SOLR_HOME_TARGET_DIR} and ${SOLR_DIST_FILE}"
+		rm -rf "${SOLR_TARGET_DIR}" "${SOLR_HOME_TARGET_DIR}" "target/${SOLR_DIST_FILE}"
 	else
 		echo "Nothing to do"
 	fi
@@ -53,16 +52,16 @@ echo "====\nDownloading and extracting Solr...\n====\n"
 (
 	cd "${SOLR_TARGET_DIR}"
 	mkdir tmp
-	curl -\# -L "${SOLR_DIST_URL}" > "tmp/solr.tgz"
+	curl -\# -L "${SOLR_DIST_URL}" > "../${SOLR_DIST_FILE}"
 	
 	echo "====== Verifying integrity ======"
 	curl -L "${SOLR_DIST_KEYS_URL}" > "tmp/KEYS" 2>/dev/null
-	curl -L "${SOLR_DIST_SIGNATURE_URL}" > "tmp/solr.tgz.asc" 2>/dev/null
+	curl -L "${SOLR_DIST_SIGNATURE_URL}" > "tmp/${SOLR_DIST_FILE}.asc" 2>/dev/null
 	
 	gpg --import "tmp/KEYS" 2> /dev/null
-	gpg --verify "tmp/solr.tgz.asc" "tmp/solr.tgz"
+	gpg --verify "tmp/${SOLR_DIST_FILE}.asc" "../${SOLR_DIST_FILE}"
 	
-	tar -zxf "tmp/solr.tgz" --strip-components 1
+	tar -zxf "../${SOLR_DIST_FILE}" --strip-components 1
 	
 	rm -rf tmp
 )
@@ -70,15 +69,9 @@ echo "====\nDownloading and extracting Solr...\n====\n"
 # Make solr home and apply configuration	
 cp -r "${SOLR_HOME_TEMPLATE_DIR}" "${SOLR_HOME_TARGET_DIR}"
 
-# Make tarball
-echo "\n====\nPackaging VLO Solr and home directory... \n====\n"
-(cd target && tar zc solr solr-home-vlo) > ${SOLR_TARGET_DIST}
-
-echo "Completed building VLO Solr instance in ${SOLR_TARGET_DIR} with configured
+echo "\nCompleted building VLO Solr instance in ${SOLR_TARGET_DIR} with configured
 Solr home in ${SOLR_HOME_TARGET_DIR}
 
-Created '${SOLR_TARGET_DIST}' for distribution
+You can start the server by running '${SOLR_TARGET_DIR}/bin/solr start -s ${SOLR_HOME_TARGET_DIR} [-t /my/solr/data/directory]'
 
-You can also start the server by running '${SOLR_TARGET_DIR}/bin/solr start -s ${SOLR_HOME_TARGET_DIR} [-t /my/solr/data/directory]'
-
-OR install Solr on your os by using the installation script in '${SOLR_TARGET_DIR}/bin."
+OR install Solr on your os by using the installation script: target/solr/bin/install_solr_service.sh target/${SOLR_DIST_FILE}"
