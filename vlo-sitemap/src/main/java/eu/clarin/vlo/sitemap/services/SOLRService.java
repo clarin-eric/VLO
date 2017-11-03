24e442e884d414033dbd2193264d9d42f2128aff
Twan Goosen <twan@clarin.eu>
2017-11-03 15:22:16 +0100

    Reformatting and slight refactoring in sitemap generator's SOLRService

--- vlo-sitemap/src/main/java/eu/clarin/vlo/sitemap/services/SOLRService.java
+++ vlo-sitemap/src/main/java/eu/clarin/vlo/sitemap/services/SOLRService.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.ximpleware.AutoPilot;
+import com.ximpleware.VTDException;
 import com.ximpleware.VTDGen;
 import com.ximpleware.VTDNav;
 
@@ -14,43 +15,43 @@ import eu.clarin.vlo.sitemap.gen.Config;
 import eu.clarin.vlo.sitemap.pojo.Sitemap.URL;
 
 public class SOLRService {
-	
-	public static final Logger _logger = LoggerFactory.getLogger(SOLRService.class);
-	
-	//to increase performances, should be increased in future
-	static final int MAX_NUM_OF_RECORDS = 1500000; //1M
-	static final String GET_IDS = "wt=xml&fl=id&rows=";
-	
-	
-	private VTDGen vg;
-	
-	public SOLRService(){
-		vg = new VTDGen();
-	}
-	
-	public List<URL> getRecordURLS() throws Exception{
 
-		List<URL> ids = new ArrayList<URL>(MAX_NUM_OF_RECORDS);
-		
-                //TODO: paginate
-		boolean parseSuccess = vg.parseHttpUrl(Config.SOLR_QUERY_URL + GET_IDS + MAX_NUM_OF_RECORDS, false);
-		
-		if(!parseSuccess)
-			throw new RuntimeException("Error retrieving or parsing result from: " + Config.SOLR_QUERY_URL + GET_IDS + MAX_NUM_OF_RECORDS);
-		
-		VTDNav nav = vg.getNav();		
-		AutoPilot ap = new AutoPilot(nav);
-				
-		ap.selectXPath("//response/result/doc/str");
-				
-		int i = -1;
-		while((i = ap.evalXPath()) != -1){
-			String id = nav.toNormalizedString(nav.getText());
-			ids.add(new URL(Config.RECORD_URL_TEMPLATE + id));
-		}
-		
-		return ids;
-			
-	}
+    public static final Logger LOGGER = LoggerFactory.getLogger(SOLRService.class);
+
+    //to increase performances, should be increased in future
+    static final int MAX_NUM_OF_RECORDS = 1500000; //1M
+    static final String GET_IDS = "wt=xml&fl=id&rows=";
+
+    private VTDGen vg;
+
+    public SOLRService() {
+        vg = new VTDGen();
+    }
+
+    public List<URL> getRecordURLS() throws VTDException {
+        final String url = Config.SOLR_QUERY_URL + GET_IDS + MAX_NUM_OF_RECORDS;
+
+        //TODO: paginate
+        final boolean parseSuccess = vg.parseHttpUrl(url, false);
+
+        if (!parseSuccess) {
+            throw new RuntimeException("Error retrieving or parsing result from: " + url);
+        }
+
+        final VTDNav nav = vg.getNav();
+        final AutoPilot ap = new AutoPilot(nav);
+
+        ap.selectXPath("//response/result/doc/str");
+
+        final List<URL> ids = new ArrayList<URL>(MAX_NUM_OF_RECORDS);
+        int i = -1;
+        while ((i = ap.evalXPath()) != -1) {
+            String id = nav.toNormalizedString(nav.getText());
+            ids.add(new URL(Config.RECORD_URL_TEMPLATE + id));
+        }
+
+        return ids;
+
+    }
 
 }
