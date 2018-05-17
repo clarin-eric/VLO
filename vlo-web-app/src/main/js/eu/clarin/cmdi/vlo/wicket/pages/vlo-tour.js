791937d0b8030a4aff152802e3d84bebca2ddbdf
Twan Goosen <twan@clarin.eu>
2018-05-17 11:13:27 +0200

    Extended tour (record page). Refs #7

--- vlo-web-app/src/main/js/eu/clarin/cmdi/vlo/wicket/pages/vlo-tour.js
+++ vlo-web-app/src/main/js/eu/clarin/cmdi/vlo/wicket/pages/vlo-tour.js
@@ -123,13 +123,19 @@ function createTourSteps() {
         }, {
             element: "#facets",
             title: "Facets",
-            content: "Various 'facets' allow you to narrow down the search results.",
+            content: "Various <em>facets</em> provide a quick overview of the available content, and allow for narrowing down the search results.",
             placement: "auto right"
         }, {
             element: "#facets .facet:first",
             title: "Expand facet",
             content: "Click the name of a facet to expand it and see the values found within the current search results. Try selecting one or more values and see how the result listing changes.",
-            placement: "auto right"
+            placement: "auto right",
+            onShown: function () {
+                //expand
+                if ($('#facets .facet:first').hasClass('collapsedfacet')) {
+                    $('#facets .facet.collapsedfacet:first a.expandable-panel-toggle').click();
+                }
+            }
         }, {
             element: "#searchresultitems .searchresultitem:first .searchresultmoreless",
             title: "Search result",
@@ -137,19 +143,19 @@ function createTourSteps() {
             placement: "auto left"
         }, {
             element: "#searchresultitems .searchresultitem:first .searchresult-licenseInfo",
-            title: "Licence information",
-            content: "Licence information, if available, is shown next to each search result.",
+            title: "Rights information",
+            content: "Basic rights information, if available, is shown next to each search result.",
             placement: "auto left"
         }, {
             element: "#searchresultitems .searchresultitem:first h3 a",
             title: "Find out more",
-            content: "Click the record title to find out everything about the described resources, including how to access the content.",
+            content: "Click the record title to find out everything about the described resources, including how to access the content. Press <strong>next</strong> to go to the record page for this result.",
             placement: "auto top"
         }, {
-            element: ".record-tabpanel",
+            element: ".record-tabpanel .nav-tabs li:last",
             title: "Record information",
             content: "Each of these tabs represents a different aspect of the record. The first tab lists the most important information about the described resource(s).",
-            placement: "auto top",
+            placement: "auto right",
             path: RegExp(/.*\/record.*/i),
             redirect: function () {
                 var resultLink = $('#searchresultitems .searchresultitem:first h3 a');
@@ -164,14 +170,53 @@ function createTourSteps() {
                 }
             }
         }, {
-            element: ".record-tabpanel",
-            title: "Resources",
+            element: ".record-tabpanel .tab1",
+            title: "Resources tab",
             content: "This tab lists the described resource(s). If present, you can click any of the links to click it. Note that not all records link directly to the described resources. In such cases, look for a landing page or search link.",
-            placement: "auto bottom",
+            placement: "auto top",
             path: RegExp(/.*\/record.*/i),
-            onShow: function() {
+            onShow: function () {
                 $(".tab1 a").click();
             }
+        }, {
+            element: ".record-tabpanel .tab2",
+            title: "Availability tab",
+            content: "Here you will find an indication of the known information regarding rights to using, accessing and/or distributing resources. Make sure to always check at the primary source before actually using or redistributing any of the resources!",
+            placement: "auto top",
+            path: RegExp(/.*\/record.*/i),
+            onShow: function () {
+                $(".tab2 a").click();
+            }
+        }, {
+            element: ".record-tabpanel .tab3",
+            title: "&quot;All metadata&quot; tab",
+            content: "Any available metadata that is not shown in the details can be found here.",
+            placement: "auto top",
+            path: RegExp(/.*\/record.*/i),
+            onShow: function () {
+                $(".tab3 a").click();
+            }
+        }, {
+            element: ".record-tabpanel .tab4",
+            title: "&quot;Technical details&quot; tab",
+            content: "Any available metadata that is not shown in the details can be found here.",
+            placement: "auto top",
+            path: RegExp(/.*\/record.*/i),
+            onShow: function () {
+                $(".tab4 a").click();
+            }
+        }, {
+            element: "#recordprevnext .btn:first",
+            title: "Search results navigation",
+            content: "Use these buttons to navigate to the previous or next item from the result results without having to go back to the list.",
+            placement: "auto bottom",
+            path: RegExp(/.*\/record.*/i)
+        }, {
+            element: "#topnavigation",
+            title: "&quot;Breadcrumbs&quot;",
+            content: "These links seve as 'breadcrumbs' that show you where you are in your exploration and allow you to go back one or more levels .",
+            placement: "auto bottom",
+            path: RegExp(/.*\/record.*/i)
         }
     ];
 
