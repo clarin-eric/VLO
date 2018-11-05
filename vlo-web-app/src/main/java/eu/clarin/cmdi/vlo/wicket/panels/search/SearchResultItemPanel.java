6dee3bfbbb15948ec2adfb68d9aabae25392b610
Twan Goosen <twan@clarin.eu>
2018-11-05 14:32:06 +0100

    Embedding of PID label in link in search results. Refs #173

--- vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/wicket/panels/search/SearchResultItemPanel.java
+++ vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/wicket/panels/search/SearchResultItemPanel.java
@@ -28,6 +28,7 @@ import eu.clarin.cmdi.vlo.service.ResourceTypeCountingService;
 import eu.clarin.cmdi.vlo.wicket.BooleanVisibilityBehavior;
 import eu.clarin.cmdi.vlo.wicket.HighlightSearchTermScriptFactory;
 import eu.clarin.cmdi.vlo.wicket.components.FacetSelectLink;
+import eu.clarin.cmdi.vlo.wicket.components.PIDLabel;
 import eu.clarin.cmdi.vlo.wicket.components.RecordPageLink;
 import eu.clarin.cmdi.vlo.wicket.components.ResourceTypeIcon;
 import eu.clarin.cmdi.vlo.wicket.components.SingleValueSolrFieldLabel;
@@ -37,7 +38,6 @@ import eu.clarin.cmdi.vlo.wicket.model.SolrDocumentExpansionPairModel;
 import eu.clarin.cmdi.vlo.wicket.model.SolrFieldModel;
 import eu.clarin.cmdi.vlo.wicket.model.SolrFieldStringModel;
 import eu.clarin.cmdi.vlo.wicket.pages.RecordPage;
-import eu.clarin.cmdi.vlo.wicket.components.PIDLinkLabel;
 import eu.clarin.cmdi.vlo.wicket.model.IsPidModel;
 import eu.clarin.cmdi.vlo.wicket.provider.ResouceTypeCountDataProvider;
 import org.apache.solr.common.SolrDocument;
@@ -234,7 +234,7 @@ public class SearchResultItemPanel extends Panel {
 
         return new WebMarkupContainer(id)
                 .add(new ExternalLink("landingPageLink", new HandleLinkModel(landingPageLinkModel))
-                        .add(new PIDLinkLabel("landingPagePidLabel", landingPageLinkModel)
+                        .add(new PIDLabel("landingPagePidLabel", landingPageLinkModel)
                                 .add(BooleanVisibilityBehavior.visibleOnTrue(isPidModel))
                         )
                         .add(new Label("landingPageLinkLabel", landingPageLinkModel) {
