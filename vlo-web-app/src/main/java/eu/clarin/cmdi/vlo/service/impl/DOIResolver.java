e70f8758647bef39acd50107e866c9e90cda294a
Twan Goosen <twan@clarin.eu>
2018-11-12 10:00:18 +0100

    DOIResolver implementation using the Handle REST client. Refs #173

--- vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/service/impl/DOIResolver.java
+++ vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/service/impl/DOIResolver.java
@@ -16,18 +16,59 @@
  */
 package eu.clarin.cmdi.vlo.service.impl;
 
+import eu.clarin.cmdi.vlo.PIDUtils;
 import eu.clarin.cmdi.vlo.service.PIDResolver;
+import eu.clarin.cmdi.vlo.service.handle.impl.HandleRestApiClient;
 import java.net.URI;
+import java.net.URISyntaxException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
+ * Resolves DOIs via the DOI handle API
  *
  * @author Twan Goosen <twan@clarin.eu>
+ * @see HandleRestApiClient
  */
 public class DOIResolver implements PIDResolver {
 
+    private final static Logger logger = LoggerFactory.getLogger(DOIResolver.class);
+
+    private final static String DOI_API_URL = "https://doi.org/api/handles/";
+    private final HandleRestApiClient handleApiClient;
+
+    public DOIResolver() {
+        handleApiClient = new HandleRestApiClient(DOI_API_URL);
+    }
+
     @Override
     public URI resolve(URI uri) {
-        return URI.create("http://dummy");
+        final String doi = PIDUtils.getSchemeSpecificId(uri.toString());
+        if (doi == null) {
+            logger.info("DOI not found in URI: {}", uri);
+            return null;
+        } else {
+            return resolveDoi(doi);
+        }
+    }
+
+    private URI resolveDoi(final String doi) {
+        final String resolved = handleApiClient.getUrl(doi);
+        if (resolved == null) {
+            logger.warn("DOI was not resolved through client: {}", doi);
+            return null;
+        } else {
+            return toUri(resolved);
+        }
+    }
+
+    private URI toUri(final String resolved) {
+        try {
+            return new URI(resolved);
+        } catch (URISyntaxException ex) {
+            logger.warn("Returned DOI is not a valid URI: {}", resolved);
+            return null;
+        }
     }
 
 }
