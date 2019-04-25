a51d8e0b1d0eac621a169f8c1a44bad9d5008049
Twan Goosen <twan@clarin.eu>
2019-04-25 16:55:29 +0200

    Optimisation + tests. Refs #220

--- vlo-importer/src/main/java/eu/clarin/cmdi/vlo/importer/AvailabilityScoreAccumulator.java
+++ vlo-importer/src/main/java/eu/clarin/cmdi/vlo/importer/AvailabilityScoreAccumulator.java
@@ -16,12 +16,15 @@
  */
 package eu.clarin.cmdi.vlo.importer;
 
+import com.google.common.base.Predicates;
 import eu.clarin.cmdi.rasa.links.CheckedLink;
 import eu.clarin.cmdi.vlo.ResourceAvailabilityScore;
 import java.net.URI;
 import java.util.Collection;
 import java.util.Map;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  *
@@ -30,50 +33,50 @@ import java.util.stream.Collectors;
 public class AvailabilityScoreAccumulator {
 
     public ResourceAvailabilityScore calculateAvailabilityScore(Map<URI, CheckedLink> linkStatusMap) {
-        if (linkStatusMap.isEmpty()) {
-            //no status information for any link
+        final Supplier<Stream<Integer>> knowStatusStreamProvider
+                = () -> linkStatusMap.values().stream()
+                        .map(CheckedLink::getStatus)
+                        .filter((status) -> (status != null && status > 0));
+
+        if (!knowStatusStreamProvider.get().findAny().isPresent()) {
+            //only unkown status information
             return ResourceAvailabilityScore.UNKNOWN;
         } else {
-            final Collection<Integer> knownStatus = linkStatusMap.values().stream()
-                    .map(CheckedLink::getStatus)
-                    .filter((status) -> (status != null && status > 0))
-                    .collect(Collectors.toList());
+            final Collection<Integer> knownStatus = knowStatusStreamProvider.get().collect(Collectors.toList());
+            return calculateScore(knownStatus);
+        }
+    }
 
-            final long knownCount = knownStatus.size();
-            if (knownCount == 0) {
-                //only status information is unknown
-                return ResourceAvailabilityScore.UNKNOWN;
-            } else {
-                //any resources that are not openly available?
-                if (knownStatus.stream().anyMatch(this::isNotAvailable)) {
-                    //no majority of restricted/procted resources; availability?
-                    final long unavailableCount = knownStatus.stream().filter(this::isNotAvailable).count();
-                    final long restrictedCount = knownStatus.stream().filter(this::isRestricted).count();
+    public ResourceAvailabilityScore calculateScore(final Collection<Integer> knownStatuses) {
+        //any resources that are not openly available?
+        if (knownStatuses.stream().anyMatch(this::isNotAvailable)) {
+            //no majority of restricted/procted resources; availability?
+            final long knownCount = knownStatuses.size();
 
-                    if (unavailableCount > restrictedCount) {
-                        //one or more unavailable but not restricted (this takes priority)
-                        if (unavailableCount == knownCount) {
-                            return ResourceAvailabilityScore.ALL_UNAVAILABLE;
-                        } else if (unavailableCount * 2 > knownCount) {
-                            return ResourceAvailabilityScore.MOST_UNAVAILABLE;
-                        } else {
-                            return ResourceAvailabilityScore.SOME_UNAVAILABLE;
-                        }
-                    } else {
-                        //all unavailable resources are restricted (i.e. no 404 etc)
-                        if (restrictedCount * 2 > knownCount) {
-                            //more than half are restricted
-                            return ResourceAvailabilityScore.MOST_RESTRICTED_ACCESS;
-                        } else {
-                            //less than half are restricted
-                            return ResourceAvailabilityScore.SOME_RESTRICTED_ACCESS;
-                        }
-                    }
+            if (knownStatuses.stream().filter(this::isNotAvailable).anyMatch(Predicates.not(this::isRestricted))) {
+                //one or more unavailable but not restricted (this takes priority)
+                final long unavailableCount = knownStatuses.stream().filter(this::isNotAvailable).count();
+                if (unavailableCount == knownCount) {
+                    return ResourceAvailabilityScore.ALL_UNAVAILABLE;
+                } else if (unavailableCount * 2 > knownCount) {
+                    return ResourceAvailabilityScore.MOST_UNAVAILABLE;
                 } else {
-                    //all are available
-                    return ResourceAvailabilityScore.ALL_AVAILABLE;
+                    return ResourceAvailabilityScore.SOME_UNAVAILABLE;
+                }
+            } else {
+                //all unavailable resources are restricted (i.e. no 404 etc)
+                final long restrictedCount = knownStatuses.stream().filter(this::isRestricted).limit(1 + knownCount / 2).count();
+                if (restrictedCount * 2 > knownCount) {
+                    //more than half are restricted
+                    return ResourceAvailabilityScore.MOST_RESTRICTED_ACCESS;
+                } else {
+                    //less than half are restricted
+                    return ResourceAvailabilityScore.SOME_RESTRICTED_ACCESS;
                 }
             }
+        } else {
+            //all are available
+            return ResourceAvailabilityScore.ALL_AVAILABLE;
         }
     }
 
