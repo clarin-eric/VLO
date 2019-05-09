5f4f79dbf86370ce8ac71e98a0a830f359edaaa9
Twan Goosen <twan@clarin.eu>
2019-05-09 11:11:25 +0200

    Tests to cover new availability score calculation cases. Refs #220

--- vlo-importer/src/test/java/eu/clarin/cmdi/vlo/importer/linkcheck/AvailabilityScoreAccumulatorTest.java
+++ vlo-importer/src/test/java/eu/clarin/cmdi/vlo/importer/linkcheck/AvailabilityScoreAccumulatorTest.java
@@ -16,14 +16,13 @@
  */
 package eu.clarin.cmdi.vlo.importer.linkcheck;
 
-import eu.clarin.cmdi.vlo.importer.linkcheck.AvailabilityScoreAccumulator;
 import eu.clarin.cmdi.rasa.links.CheckedLink;
 import eu.clarin.cmdi.vlo.ResourceAvailabilityScore;
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotEquals;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -46,60 +45,127 @@ public class AvailabilityScoreAccumulatorTest {
     public void testCalculateAvailabilityScoreNoData() {
         assert map.isEmpty();
 
-        ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size());
-        assertEquals(ResourceAvailabilityScore.UNKNOWN, result);
+        {
+            final ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size());
+            assertEquals(ResourceAvailabilityScore.UNKNOWN, result);
+        }
+
+        // total number of resources should not make a difference
+        {
+            final ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, 100);
+            assertEquals(ResourceAvailabilityScore.UNKNOWN, result);
+        }
     }
 
     @Test
     public void testCalculateAvailabilityScoreAllUnknown() {
         addStatus(0, 0);
         assert map.size() == 2;
-        
-        ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size());
-        assertEquals(ResourceAvailabilityScore.UNKNOWN, result);
+
+        {
+            final ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size());
+            assertEquals(ResourceAvailabilityScore.UNKNOWN, result);
+        }
+
+        // total number of resources should not make a difference
+        {
+            ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, 100);
+            assertEquals(ResourceAvailabilityScore.UNKNOWN, result);
+        }
+    }
+
+    @Test
+    public void testCalculateAvailabilityScoreEffecitvelyUnknown() {
+        addStatus(200, 200);
+        assert map.size() == 2;
+
+        {
+            // all available but too few known statuses compared to total number of resources
+            final ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, 100);
+            assertEquals(ResourceAvailabilityScore.UNKNOWN, result);
+        }
+
+        addStatus(0, 0, 0);
+        {
+            // too few known statuses compared to total number of resources (although it equals map size)
+            final ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size());
+            assertEquals(ResourceAvailabilityScore.UNKNOWN, result);
+        }
     }
 
     @Test
     public void testCalculateAvailabilityScoreAllAvailable() {
         addStatus(200, 200, 301, 302, 303);
         assert map.size() == 5;
-        
-        ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size());
-        assertEquals(ResourceAvailabilityScore.ALL_AVAILABLE, result);
+
+        {
+            ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size());
+            assertEquals(ResourceAvailabilityScore.ALL_AVAILABLE, result);
+        }
+
+        //adding unknown should NOT result in 'all unavailable'
+        {
+            ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size() * 2);
+            assertNotEquals(ResourceAvailabilityScore.ALL_AVAILABLE, result);
+        }
     }
 
     @Test
     public void testCalculateAvailabilityScoreAllUnavailable() {
         addStatus(400, 403, 404, 500);
         assert map.size() == 4;
-        
-        ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size());
-        assertEquals(ResourceAvailabilityScore.ALL_UNAVAILABLE, result);
+
+        {
+            ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size());
+            assertEquals(ResourceAvailabilityScore.ALL_UNAVAILABLE, result);
+        }
+
+        //adding one unknown should NOT result in 'all unavailable'
+        {
+            ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size() + 1);
+            assertNotEquals(ResourceAvailabilityScore.ALL_UNAVAILABLE, result);
+        }
     }
 
     @Test
     public void testCalculateAvailabilityScoreMostUnavailable() {
         addStatus(400, 403, 500, 200, 301);
         assert map.size() == 5;
-        
-        ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size());
-        assertEquals(ResourceAvailabilityScore.MOST_UNAVAILABLE, result);
+
+        {
+            ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size());
+            assertEquals(ResourceAvailabilityScore.MOST_UNAVAILABLE, result);
+        }
+
+        //adding one unknown should NOT result in 'most unavailable'
+        {
+            ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size() + 1);
+            assertNotEquals(ResourceAvailabilityScore.MOST_UNAVAILABLE, result);
+        }
     }
-    
+
     @Test
     public void testCalculateAvailabilityScoreMostRestricted() {
         addStatus(401, 401, 403, 200, 200);
         assert map.size() == 5;
-        
-        ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size());
-        assertEquals(ResourceAvailabilityScore.MOST_RESTRICTED_ACCESS, result);
+
+        {
+            ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size());
+            assertEquals(ResourceAvailabilityScore.MOST_RESTRICTED_ACCESS, result);
+        }
+
+        //adding one unknown should NOT result in 'most restricted'
+        {
+            ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size() + 1);
+            assertNotEquals(ResourceAvailabilityScore.MOST_RESTRICTED_ACCESS, result);
+        }
     }
-    
+
     @Test
     public void testCalculateAvailabilityScoreMostUnavailableWithRestricted() {
         addStatus(401, 401, 403, 404, 200);
         assert map.size() == 5;
-        
+
         ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size());
         assertEquals(ResourceAvailabilityScore.MOST_UNAVAILABLE, result);
     }
@@ -108,7 +174,7 @@ public class AvailabilityScoreAccumulatorTest {
     public void testCalculateAvailabilityScoreSomeUnavailable() {
         addStatus(400, 404, 200, 301);
         assert map.size() == 4;
-        
+
         ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size());
         assertEquals(ResourceAvailabilityScore.SOME_UNAVAILABLE, result);
     }
@@ -117,16 +183,16 @@ public class AvailabilityScoreAccumulatorTest {
     public void testCalculateAvailabilityScoreSomeUnavailableWithRestricted() {
         addStatus(400, 403, 200, 301);
         assert map.size() == 4;
-        
+
         ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size());
         assertEquals(ResourceAvailabilityScore.SOME_UNAVAILABLE, result);
     }
-    
+
     @Test
     public void testCalculateAvailabilityScoreSomeRestricted() {
         addStatus(401, 403, 200, 301, 303);
         assert map.size() == 5;
-        
+
         ResourceAvailabilityScore result = instance.calculateAvailabilityScore(map, map.size());
         assertEquals(ResourceAvailabilityScore.SOME_RESTRICTED_ACCESS, result);
     }
