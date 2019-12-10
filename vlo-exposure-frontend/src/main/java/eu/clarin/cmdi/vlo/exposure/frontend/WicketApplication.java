7d6d6978a93472ebbc8f69b2676fc96a122def6d
Twan Goosen <twan@clarin.eu>
2019-12-10 14:00:32 +0100

    some improvements in exposure front end

--- vlo-exposure-frontend/src/main/java/eu/clarin/cmdi/vlo/exposure/frontend/WicketApplication.java
+++ vlo-exposure-frontend/src/main/java/eu/clarin/cmdi/vlo/exposure/frontend/WicketApplication.java
@@ -7,24 +7,23 @@ import eu.clarin.cmdi.vlo.config.VloConfig;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Optional;
 import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.wicket.Application;
 
-import eu.clarin.cmdi.vlo.exposure.frontend.Record;
-import eu.clarin.cmdi.vlo.exposure.frontend.HomePage;
-
 /**
  * Application object for your web application. If you want to run this
  * application without deploying, run the Start class.
- * 
+ *
  * @see eu.clarin.cmdi.Start#main(String[])
  */
 public class WicketApplication extends WebApplication {
+
     private VloConfig vloConfig;
     private final String configProperties = "./vlo-exposure-frontend/config.properties"; //System.getProperty("user.dir")+
-    
+
     private final static Logger logger = LoggerFactory.getLogger(WicketApplication.class);
 
     /**
@@ -44,23 +43,32 @@ public class WicketApplication extends WebApplication {
         initVloConfig();
         mountPages();
     }
-    
+
     private void initVloConfig() {
         try {
-            final Properties properties = new Properties();
 
-            System.out.println(this.configProperties);
-            properties.load(new FileReader(this.configProperties));
-            final File configUrl = new File(properties.getProperty("vlo.config.file"));
-            final XmlVloConfigFactory configFactory = new XmlVloConfigFactory(configUrl.toURI().toURL());
+            final Optional<String> configLocation
+                    = Optional.ofNullable(
+                            getServletContext().getInitParameter("eu.carlin.cmdi.vlo.config.location"));
+
+            final File configFile = configLocation.map(File::new).orElseGet(() -> {
+                final Properties properties = new Properties();
+                try (FileReader fr = new FileReader(configProperties)) {
+                    properties.load(fr);
+                    return new File(properties.getProperty("vlo.config.file"));
+                } catch (IOException ex) {
+                    throw new RuntimeException("Could not read properties file " + configProperties, ex);
+                }
+            });
+
+            final XmlVloConfigFactory configFactory = new XmlVloConfigFactory(configFile.toURI().toURL());
             this.vloConfig = configFactory.newConfig();
-        }catch(Exception ex) {
-            //logger.error(ex.getMessage());
-            System.out.print(ex.getMessage());
+        } catch (IOException ex) {
+            logger.error("Failed to initialise", ex);
         }
-        
+
     }
-    
+
     public static WicketApplication get() {
         return (WicketApplication) Application.get();
     }
@@ -68,7 +76,7 @@ public class WicketApplication extends WebApplication {
     public VloConfig getConfig() {
         return this.vloConfig;
     }
-    
+
     private void mountPages() {
         // frontend
         mountPage("/index", HomePage.class);
