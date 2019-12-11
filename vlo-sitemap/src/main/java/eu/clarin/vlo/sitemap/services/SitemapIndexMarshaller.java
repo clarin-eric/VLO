6461ee686cace6f98552f93282d93ad5fb992453
Twan Goosen <twan@clarin.eu>
2019-12-11 10:03:22 +0100

    Sitemap generator error handling improved/simplified

--- vlo-sitemap/src/main/java/eu/clarin/vlo/sitemap/services/SitemapIndexMarshaller.java
+++ vlo-sitemap/src/main/java/eu/clarin/vlo/sitemap/services/SitemapIndexMarshaller.java
@@ -10,22 +10,24 @@ import org.slf4j.LoggerFactory;
 
 import eu.clarin.vlo.sitemap.gen.Config;
 import eu.clarin.vlo.sitemap.pojo.SitemapIndex;
+import java.io.IOException;
+import javax.xml.bind.JAXBException;
 
 public class SitemapIndexMarshaller {
-	
-	static Logger _logger = LoggerFactory.getLogger(SitemapIndexMarshaller.class);
-	
-	public void marshall(SitemapIndex index) throws Exception{
-		_logger.info("Generating index");
-		
-		String fileName = Config.OUTPUT_FOLDER + "/" + Config.SITEMAP_INDEX_NAME;
-		
-		JAXBContext jaxbContext = JAXBContext.newInstance(SitemapIndex.class);
-		Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-		jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-		jaxbMarshaller.marshal(index, new File(fileName));
-		
-		_logger.info("Finished");
-	}	
+
+    static Logger _logger = LoggerFactory.getLogger(SitemapIndexMarshaller.class);
+
+    public void marshall(SitemapIndex index) throws IOException, JAXBException {
+        _logger.info("Generating index");
+
+        String fileName = Config.OUTPUT_FOLDER + "/" + Config.SITEMAP_INDEX_NAME;
+
+        JAXBContext jaxbContext = JAXBContext.newInstance(SitemapIndex.class);
+        Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        jaxbMarshaller.marshal(index, new File(fileName));
+
+        _logger.info("Finished");
+    }
 
 }
