8f3de44a4f7c3b732d28922f3d053453519b6b67
Twan Goosen <twan@clarin.eu>
2020-12-17 09:57:31 +0100

    small cleanup

--- vlo-monitor/src/main/java/eu/clarin/cmdi/vlo/monitor/MonitorApplication.java
+++ vlo-monitor/src/main/java/eu/clarin/cmdi/vlo/monitor/MonitorApplication.java
@@ -1,6 +1,5 @@
 package eu.clarin.cmdi.vlo.monitor;
 
-import eu.clarin.cmdi.vlo.config.VloConfigFactory;
 import eu.clarin.cmdi.vlo.monitor.service.VloMonitor;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
@@ -16,9 +15,6 @@ public class MonitorApplication {
 
     @Inject
     private VloMonitor vloMonitor;
-
-    @Inject
-    private VloConfigFactory vloConfigFactory;
 
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
