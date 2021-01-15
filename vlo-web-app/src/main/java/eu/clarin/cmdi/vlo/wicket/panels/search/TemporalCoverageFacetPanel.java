69e664b9ed373a745ec857e227b72fd49dea1b95
Twan Goosen <twan@clarin.eu>
2021-01-15 11:16:02 +0100

    Refactoring and ajax form for temporal coverage

--- vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/wicket/panels/search/TemporalCoverageFacetPanel.java
+++ vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/wicket/panels/search/TemporalCoverageFacetPanel.java
@@ -50,6 +50,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.markup.html.form.AjaxCheckBox;
+import org.apache.wicket.ajax.markup.html.form.AjaxFallbackButton;
 import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.model.LambdaModel;
 import org.apache.wicket.model.LoadableDetachableModel;
@@ -64,29 +65,27 @@ import org.slf4j.LoggerFactory;
  * @author Twan Goosen
  */
 public abstract class TemporalCoverageFacetPanel extends ExpandablePanel<QueryFacetsSelection> implements IAjaxIndicatorAware {
-    
+
     private final static Logger logger = LoggerFactory.getLogger(TemporalCoverageFacetPanel.class);
-    
+
     @SpringBean
     private FieldNameService fieldNameService;
-    
+
     private final String TEMPORAL_COVERAGE = fieldNameService.getFieldName(FieldKey.TEMPORAL_COVERAGE);
     private final IModel<FacetSelectionType> selectionTypeModeModel;
     private final AjaxIndicatorAppender indicatorAppender = new AjaxIndicatorAppender();
     private final IModel<TemporalCoverageRange> temporalCoverageRangeModel;
-    private final TemporalCoverageAjaxRangeSlider slider;
-    
+
     private final IModel<Boolean> filterEnabledModel;
-    private final AjaxCheckBox enabledToggle;
-    
+
     private final static Pattern RANGE_PATTERN = Pattern.compile("\\[(\\d+) TO (\\d+)\\]");
     private final static int RANGE_PATTERN_LOWER_GROUP = 1;
     private final static int RANGE_PATTERN_UPPER_GROUP = 2;
-    
+
     private IModel<Optional<Integer>> upperModel;
     private IModel<Optional<Integer>> lowerModel;
     private final IModel<RangeValue> rangeModel;
-    
+
     public TemporalCoverageFacetPanel(String id,
             final IModel<QueryFacetsSelection> selectionModel,
             final IModel<FacetSelectionType> selectionTypeModeModel,
@@ -95,57 +94,49 @@ public abstract class TemporalCoverageFacetPanel extends ExpandablePanel<QueryFa
         this.temporalCoverageRangeModel = temporalCoverageRangeModel;
         this.selectionTypeModeModel = selectionTypeModeModel;
         this.filterEnabledModel = new Model(false);
-        
-        final FeedbackPanel feedback = new JQueryFeedbackPanel("feedbackTemporalCoverage");
-        
+
         lowerModel = LambdaModel.of(
                 () -> getBoundFromSelection(selectionModel, RANGE_PATTERN_LOWER_GROUP, () -> temporalCoverageRangeModel.getObject().getStart()),
                 (lower) -> {
                     applySelection(lower, upperModel.getObject());
                 }
         );
-        
+
         upperModel = LambdaModel.of(
                 () -> getBoundFromSelection(selectionModel, RANGE_PATTERN_UPPER_GROUP, () -> temporalCoverageRangeModel.getObject().getEnd()),
                 (upper) -> {
                     applySelection(lowerModel.getObject(), upper);
                 }
         );
-        
+
         rangeModel = new RangeValueModel(lowerModel, upperModel);
+
+        final Form<RangeValue> form = createTemporalCoverageForm("temporalCoverage", temporalCoverageRangeModel);
+        add(form);
+    }
+
+    private Form<RangeValue> createTemporalCoverageForm(String id, IModel<TemporalCoverageRange> temporalCoverageRangeModel1) {
+        final Form<RangeValue> form = new Form<>(id, rangeModel);
         
+        // Form has its own feedback panel
+        final FeedbackPanel feedback = new JQueryFeedbackPanel("feedbackTemporalCoverage");
+        form.add(feedback.setOutputMarkupId(true));
+
+        // Inputs to determine range: text boxes for upper/lower bound
         final TextField<Integer> lowerInput = new TextField<>("lower", new UnwrappedOptionalModel<>(lowerModel));
         lowerInput.setType(Integer.class);
+        form.add(lowerInput);
+
         final TextField<Integer> upperInput = new TextField<>("upper", new UnwrappedOptionalModel<>(upperModel));
         upperInput.setType(Integer.class);
-        
-        slider = new TemporalCoverageAjaxRangeSlider("slider", rangeModel, lowerInput, upperInput, feedback);
-        
-        final Form<RangeValue> form = new Form<>("temporalCoverage", rangeModel);
-        form.add(feedback.setOutputMarkupId(true));
-        form.add(new Behavior() {
-            @Override
-            public void onConfigure(Component component) {
-                final Optional<Integer> start = temporalCoverageRangeModel.getObject().getStart();
-                final Optional<Integer> end = temporalCoverageRangeModel.getObject().getEnd();
-                
-                slider
-                        .setDisabled(!filterEnabledModel.getObject())
-                        .setMin(start.orElse(null))
-                        .setMax(end.orElse(null))
-                        .setRangeValidator(new RangeValidator<>(start.orElse(-10000), end.orElse(10000)));
-                lowerInput.setEnabled(filterEnabledModel.getObject());
-                upperInput.setEnabled(filterEnabledModel.getObject());
-                enabledToggle.setEnabled(!start.isEmpty() || !end.isEmpty());
-            }
-            
-        });
-        
-        form.add(lowerInput);
         form.add(upperInput);
+
+        // Inputs to determine range: slider
+        final TemporalCoverageAjaxRangeSlider slider = new TemporalCoverageAjaxRangeSlider("slider", rangeModel, lowerInput, upperInput, feedback);
         form.add(slider);
-        
-        enabledToggle = new AjaxCheckBox("filterEnabled", filterEnabledModel) {
+
+        // Checkbox to enable/disable filter
+        final AjaxCheckBox enabledToggle = new AjaxCheckBox("filterEnabled", filterEnabledModel) {
             @Override
             protected void onUpdate(AjaxRequestTarget target) {
                 applySelection(lowerModel.getObject(), upperModel.getObject());
@@ -155,13 +146,43 @@ public abstract class TemporalCoverageFacetPanel extends ExpandablePanel<QueryFa
                 }
             }
         };
-        
         form.add(enabledToggle);
-        
+
+        // Make submit button an Ajax enabled one to prevent page reload on submit
+        form.add(new AjaxFallbackButton("submit", form) {
+            @Override
+            protected void onSubmit(Optional<AjaxRequestTarget> target) {
+                selectionChanged(target);
+                target.ifPresent(t -> {
+                    t.add(form);
+                });
+            }
+
+        });
+
+        // Synchronise the form controls with the models
+        form.add(new Behavior() {
+            @Override
+            public void onConfigure(Component component) {
+                final Optional<Integer> start = temporalCoverageRangeModel1.getObject().getStart();
+                final Optional<Integer> end = temporalCoverageRangeModel1.getObject().getEnd();
+                slider
+                        .setDisabled(!filterEnabledModel.getObject())
+                        .setMin(start.orElse(null))
+                        .setMax(end.orElse(null))
+                        .setRangeValidator(new RangeValidator<>(start.orElse(-10000), end.orElse(10000)));
+                lowerInput.setEnabled(filterEnabledModel.getObject());
+                upperInput.setEnabled(filterEnabledModel.getObject());
+                enabledToggle.setEnabled(!start.isEmpty() || !end.isEmpty());
+            }
+        });
+
+        // Show spinner while busy...
         form.add(indicatorAppender);
-        add(form);
+
+        return form;
     }
-    
+
     private void applySelection(Optional<Integer> lower, Optional<Integer> upper) {
         if (filterEnabledModel.getObject()) {
             final String sel = "[" + lower.map(Objects::toString).orElse("*") + " TO " + upper.map(Objects::toString).orElse("*") + "]";
@@ -170,13 +191,13 @@ public abstract class TemporalCoverageFacetPanel extends ExpandablePanel<QueryFa
             getModelObject().removeFacetSelection(TEMPORAL_COVERAGE);
         }
     }
-    
+
     private Optional<Integer> getBoundFromSelection(final IModel<QueryFacetsSelection> selectionModel, final int rangeMatchGroup, Supplier<Optional<Integer>> defaultProvider) {
         Optional<String> coverageSelection = Optional.ofNullable(selectionModel.getObject())
                 .flatMap(selection -> Optional.ofNullable(selection.getSelectionValues(TEMPORAL_COVERAGE)))
                 .map(FacetSelection::getValues)
                 .flatMap(values -> values.size() == 1 ? Optional.of(values.iterator().next()) : Optional.empty());
-        
+
         if (coverageSelection.isEmpty()) {
             return defaultProvider.get();
         } else {
@@ -197,90 +218,90 @@ public abstract class TemporalCoverageFacetPanel extends ExpandablePanel<QueryFa
             );
         }
     }
-    
+
     @Override
     protected void onConfigure() {
         super.onConfigure();
-        
+
         if (getModelObject().getSelectionValues(TEMPORAL_COVERAGE) != null) {
             //if there any selection, make initially expanded
             getExpansionModel().setObject(ExpansionState.EXPANDED);
         }
     }
-    
+
     @Override
     public void renderHead(IHeaderResponse response) {
         super.renderHead(response);
     }
-    
+
     @Override
     protected Label createTitleLabel(String id) {
         return new Label(id, new StringResourceModel("temporalCoverageTitle"));
     }
-    
+
     @Override
     public void detachModels() {
         super.detachModels();
-        
+
         if (this.temporalCoverageRangeModel != null) {
             this.temporalCoverageRangeModel.detach();
         }
-        
+
         if (selectionTypeModeModel != null) {
             this.selectionTypeModeModel.detach();
         }
     }
-    
+
     @Override
     public String getAjaxIndicatorMarkupId() {
         return indicatorAppender.getMarkupId();
     }
-    
+
     protected abstract void selectionChanged(Optional<AjaxRequestTarget> target);
-    
+
     private static class RangeValueModel extends LoadableDetachableModel<RangeValue> {
-        
+
         private final IModel<Optional<Integer>> lowerModel;
         private final IModel<Optional<Integer>> upperModel;
-        
+
         public RangeValueModel(IModel<Optional<Integer>> lowerModel, IModel<Optional<Integer>> upperModel) {
             this.lowerModel = lowerModel;
             this.upperModel = upperModel;
         }
-        
+
         @Override
         protected RangeValue load() {
             return new RangeValue(
                     lowerModel.orElse(Optional.empty()).getObject().orElse(null),
                     upperModel.orElse(Optional.empty()).getObject().orElse(null));
         }
-        
+
         @Override
         public void setObject(RangeValue object) {
             super.setObject(object);
             lowerModel.setObject(Optional.of(object.getLower()));
             upperModel.setObject(Optional.of(object.getUpper()));
         }
-        
+
     }
-    
+
     private class TemporalCoverageAjaxRangeSlider extends AjaxRangeSlider {
-        
+
         private final FeedbackPanel feedback;
-        
+
         public TemporalCoverageAjaxRangeSlider(String id, IModel<RangeValue> model, TextField<Integer> lower, TextField<Integer> upper, FeedbackPanel feedback) {
             super(id, model, lower, upper);
             this.feedback = feedback;
         }
         private static final long serialVersionUID = 1L;
-        
+
         @Override
         protected void onError(AjaxRequestTarget target) {
             if (target != null) {
                 target.add(feedback); // do never add 'this' or the form here!
             }
         }
-        
+
         @Override
         public void onValueChanged(IPartialPageRequestHandler handler) {
             if (handler instanceof AjaxRequestTarget) {
@@ -290,11 +311,11 @@ public abstract class TemporalCoverageFacetPanel extends ExpandablePanel<QueryFa
                 selectionChanged(Optional.empty());
             }
         }
-        
+
         public TemporalCoverageAjaxRangeSlider setDisabled(boolean disabled) {
             options.set("disabled", disabled);
             return this;
         }
     }
-    
+
 }
