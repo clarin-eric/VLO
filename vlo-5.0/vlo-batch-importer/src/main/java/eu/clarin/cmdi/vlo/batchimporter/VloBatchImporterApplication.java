32495eae4492977d40e797915857870ef2af1b6a
Twan Goosen <twan@clarin.eu>
2021-08-18 14:53:13 +0300

    added vlo batch importer

---
+++ vlo-5.0/vlo-batch-importer/src/main/java/eu/clarin/cmdi/vlo/batchimporter/VloBatchImporterApplication.java
@@ -0,0 +1,13 @@
+package eu.clarin.cmdi.vlo.batchimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VloBatchImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VloBatchImporterApplication.class, args);
+	}
+
+}
---
+++ vlo-5.0/vlo-batch-importer/src/test/java/eu/clarin/cmdi/vlo/batchimporter/VloBatchImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.clarin.cmdi.vlo.batchimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VloBatchImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
