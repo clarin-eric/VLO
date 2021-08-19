621bf7735589d3ca3c076363fde6c71586c8802b
Twan Goosen <twan@clarin.eu>
2021-08-19 12:23:43 +0300

    mocking up api

--- vlo-5.0/vlo-api/src/main/java/eu/clarin/cmdi/vlo/api/VloApiApplication.java
+++ vlo-5.0/vlo-api/src/main/java/eu/clarin/cmdi/vlo/api/VloApiApplication.java
@@ -2,12 +2,14 @@ package eu.clarin.cmdi.vlo.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class VloApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VloApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(VloApiApplication.class, args);        
+
+    }
 
 }
