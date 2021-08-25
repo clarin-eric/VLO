be567cd687bf96abef659bd209e2c6c14471550e
Twan Goosen <twan@clarin.eu>
2021-08-25 10:36:24 +0300

    VLO application: elastic connection configuration

--- vlo-5.0/vlo-api/src/main/java/eu/clarin/cmdi/vlo/api/VloApiApplication.java
+++ vlo-5.0/vlo-api/src/main/java/eu/clarin/cmdi/vlo/api/VloApiApplication.java
@@ -1,15 +1,19 @@
 package eu.clarin.cmdi.vlo.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.elasticsearch.repository.config.EnableReactiveElasticsearchRepositories;
 
 @SpringBootApplication
+@EnableReactiveElasticsearchRepositories
+@Slf4j
 public class VloApiApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(VloApiApplication.class, args);        
-
+        log.info("VLO API: starting");
+        SpringApplication.run(VloApiApplication.class, args);
+        log.info("VLO API: done");
     }
 
 }
