430b3c01a850207eef84c3583574f6e6faadce0c
Twan Goosen <twan@clarin.eu>
2021-10-26 16:55:42 +0300

    Removed unused VloRecordIndexService

--- vlo-5.0/vlo-api/src/test/java/eu/clarin/cmdi/vlo/api/data/VloRecordRepositoryIntegrationTest.java
+++ vlo-5.0/vlo-api/src/test/java/eu/clarin/cmdi/vlo/api/data/VloRecordRepositoryIntegrationTest.java
@@ -18,10 +18,13 @@ package eu.clarin.cmdi.vlo.api.data;
 
 import com.google.common.collect.Lists;
 import eu.clarin.cmdi.vlo.data.model.VloRecord;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.function.BooleanSupplier;
 import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.Requests;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -37,7 +40,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
-import org.springframework.data.elasticsearch.client.reactive.ReactiveElasticsearchClient;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableReactiveElasticsearchRepositories;
 import org.springframework.test.context.junit.jupiter.EnabledIf;
@@ -46,7 +48,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
- * 
+ *
  * @author CLARIN ERIC <clarin@clarin.eu>
  */
 @ExtendWith(SpringExtension.class)
@@ -55,14 +57,11 @@ import reactor.core.publisher.Mono;
 @EnableReactiveElasticsearchRepositories(basePackageClasses = VloRecordRepository.class)
 @ImportAutoConfiguration({ReactiveElasticsearchRestClientAutoConfiguration.class, ElasticsearchDataAutoConfiguration.class})
 @Slf4j
-public class VloRecordIndexServiceTest {
+public class VloRecordRepositoryIntegrationTest {
 
     @Autowired
-    private VloRecordRepository recordRepository;
+    private VloRecordRepository instance;
 
-    @Autowired
-    private ReactiveElasticsearchClient reactiveElasticsearchClient;
-    
     @Autowired
     private RestHighLevelClient elasticsearchClient;
 
@@ -70,7 +69,7 @@ public class VloRecordIndexServiceTest {
 
     @AfterEach
     public void cleanUp() {
-        if (elasticChecker().getAsBoolean()) {
+        if (checkElasticConnection().getAsBoolean()) {
             Flux.fromIterable(insertedIds)
                     .parallel()
                     .doOnNext(s -> {
@@ -80,7 +79,7 @@ public class VloRecordIndexServiceTest {
                         log.warn("Error while cleaning up from index", e);
                     })
                     .flatMap(id -> {
-                        return recordRepository.deleteById(id)
+                        return instance.deleteById(id)
                                 .then(Mono.fromCallable(() -> insertedIds.remove(id)));
                     })
                     .then()
@@ -88,26 +87,20 @@ public class VloRecordIndexServiceTest {
         }
     }
 
-    private BooleanSupplier elasticChecker() {
-        return () -> elasticsearchClient.getLowLevelClient().isRunning();
-    }
-
     /**
      * Test of sendToIndex method, of class VloRecordIndexService.
      */
     @Test
-    public void testSendToIndex() {
-        Assumptions.assumeTrue(elasticChecker(), "ElasticSearch not running. SKIPPING!");
+    public void testSave() throws IOException {
+        Assumptions.assumeTrue(checkElasticConnection(), "ElasticSearch not running. SKIPPING!");
 
         final VloRecord inputRecord = VloRecord.builder()
                 .id("id123")
                 .dataRoot("testDataRoot")
                 .build();
 
-        final VloRecordIndexService instance = new VloRecordIndexService(reactiveElasticsearchClient);
-
-        Mono<IndexResponse> result = instance.sendToIndex(Mono.just(inputRecord));
-        final IndexResponse response = result
+        final Mono<VloRecord> result = instance.save(inputRecord);
+        final VloRecord response = result
                 .doOnError((e) -> {
                     fail("Error in IndexResponse mono", e);
                 })
@@ -116,28 +109,27 @@ public class VloRecordIndexServiceTest {
         assertNotNull(response);
         insertedIds.add(response.getId());
 
-        final VloRecord foundRecord = recordRepository
-                .findById(response.getId())
-                .doOnError((e) -> {
-                    fail("Error retrieving record mono", e);
-                })
-                .block();
-        assertNotNull(foundRecord);
-//        assertEquals(inputRecord.getDataRoot(), foundRecord.getDataRoot());
-//        assertEquals(inputRecord.getId(), foundRecord.getId());
+        GetResponse getResponse = elasticsearchClient.get(
+                Requests.getRequest("record")
+                        .id(inputRecord.getId()),
+                RequestOptions.DEFAULT);
+        assertEquals(inputRecord.getId(), getResponse.getId());
+        assertEquals(inputRecord.getDataRoot(), getResponse.getSourceAsMap().get("dataRoot"));
+    }
+
+    private BooleanSupplier checkElasticConnection() {
+        return () -> elasticsearchClient.getLowLevelClient().isRunning();
     }
 
     @Configuration
     public static class TestElasticsearchConfig extends AbstractElasticsearchConfiguration {
 
+        @Autowired
+        private ClientConfiguration clientConfiguration;
+
         @Override
         @Bean
         public RestHighLevelClient elasticsearchClient() {
-
-            final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                    .connectedTo("localhost:9200")
-                    .build();
-
             return RestClients.create(clientConfiguration).rest();
         }
     }
