21bf33843e884ac564ed0803b3a71c6293dd4aaf
Twan Goosen <twan@clarin.eu>
2021-11-01 10:45:08 +0200

    vlo-web-app project initialised

---
+++ vlo-5.0/vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/web/VloWebAppApplication.java
@@ -0,0 +1,13 @@
+package eu.clarin.cmdi.vlo.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VloWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VloWebAppApplication.class, args);
+	}
+
+}
