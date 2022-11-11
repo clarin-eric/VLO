b06a32ed43c2e354d67cbe5cbeffcc8ca97cec5a
Twan Goosen <twan@clarin.eu>
2022-11-11 11:51:41 +0100

    different replacement behaviours for value map transformer

--- vlo-5.0/vlo-mapping/src/test/java/eu/clarin/cmdi/vlo/mapping/rules/transformation/ValueMapTransformerTest.java
+++ vlo-5.0/vlo-mapping/src/test/java/eu/clarin/cmdi/vlo/mapping/rules/transformation/ValueMapTransformerTest.java
@@ -24,6 +24,7 @@ import eu.clarin.cmdi.vlo.mapping.VloMappingConfiguration;
 import eu.clarin.cmdi.vlo.mapping.model.SimpleValueContext;
 import eu.clarin.cmdi.vlo.mapping.model.ValueContext;
 import eu.clarin.cmdi.vlo.mapping.model.ValueLanguagePair;
+import eu.clarin.cmdi.vlo.mapping.rules.transformation.ValueMapTransformer.ValueMappingBehaviour;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -35,7 +36,6 @@ import static org.hamcrest.Matchers.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -114,6 +114,81 @@ public class ValueMapTransformerTest {
      * Test of apply method, of class ValueMapTransformer.
      */
     @Test
+    public void testSimpleMapCaseInsensitiveAddOnMatch() {
+        instance.setMap(SIMPLE_VALUE_MAP);
+        instance.setCaseSensitive(false);
+        instance.setRegex(false);
+        instance.setTargetLang("en");
+        instance.setBehaviour(ValueMappingBehaviour.ADD_ON_MATCH);
+
+        final ValueContext valueContext = SimpleValueContext.builder()
+                .values(ImmutableList.of(
+                        new ValueLanguagePair("a", "x"),
+                        new ValueLanguagePair("B", "fr"),
+                        new ValueLanguagePair("c", null),
+                        new ValueLanguagePair("d", "x")))
+                .build();
+
+        final Stream<ValueLanguagePair> resultStream = instance.apply(valueContext, mappingConfig);
+        assertNotNull(resultStream);
+
+        // collect
+        final List<ValueLanguagePair> result = resultStream.toList();
+        assertThat(result, hasSize(7));
+
+        assertThat("Original values should be included", result, hasItems(
+                allOf(hasProperty("value", equalTo("a")), hasProperty("language", equalTo("x"))),
+                allOf(hasProperty("value", equalTo("B")), hasProperty("language", equalTo("fr"))),
+                allOf(hasProperty("value", equalTo("c"))),
+                allOf(hasProperty("value", equalTo("d")), hasProperty("language", equalTo("x")))));
+
+        assertThat("Full match including case should be mapped", result, hasItems(
+                allOf(hasProperty("value", equalTo("AA")), hasProperty("language", equalTo("en"))),
+                allOf(hasProperty("value", equalTo("CC")), hasProperty("language", equalTo("en")))));
+
+        assertThat("Case insensitive match should be mapped", result, hasItems(
+                allOf(hasProperty("value", equalTo("BB")), hasProperty("language", equalTo("en")))));
+    }
+
+    /**
+     * Test of apply method, of class ValueMapTransformer.
+     */
+    @Test
+    public void testSimpleMapCaseInsensitiveRemoveOriginal() {
+        instance.setMap(SIMPLE_VALUE_MAP);
+        instance.setCaseSensitive(false);
+        instance.setRegex(false);
+        instance.setTargetLang("en");
+        instance.setBehaviour(ValueMappingBehaviour.REMOVE_ORIGINAL);
+
+        final ValueContext valueContext = SimpleValueContext.builder()
+                .values(ImmutableList.of(
+                        new ValueLanguagePair("a", "x"),
+                        new ValueLanguagePair("B", "fr"),
+                        new ValueLanguagePair("c", null),
+                        new ValueLanguagePair("d", "x")))
+                .build();
+
+        final Stream<ValueLanguagePair> resultStream = instance.apply(valueContext, mappingConfig);
+        assertNotNull(resultStream);
+
+        // collect
+        final List<ValueLanguagePair> result = resultStream.toList();
+        assertThat(result, hasSize(3));
+
+        assertThat("Non-matching item should NOT be kept", result,
+                not(hasItem(hasProperty("value", equalTo("d")))));
+
+        assertThat("Full match including case should be mapped", result, hasItems(
+                allOf(hasProperty("value", equalTo("AA")), hasProperty("language", equalTo("en"))),
+                allOf(hasProperty("value", equalTo("BB")), hasProperty("language", equalTo("en"))),
+                allOf(hasProperty("value", equalTo("CC")), hasProperty("language", equalTo("en")))));
+    }
+
+    /**
+     * Test of apply method, of class ValueMapTransformer.
+     */
+    @Test
     public void testSimpleMapCaseSensitive() {
         instance.setMap(SIMPLE_VALUE_MAP);
         instance.setCaseSensitive(true);
@@ -183,11 +258,13 @@ public class ValueMapTransformerTest {
      * Test of apply method, of class ValueMapTransformer.
      */
     @Test
-    public void testRegexMapCaseInsensitive() {
+    public void testRegexMapCaseInsensitiveDefaultBehaviour() {
         instance.setMap(REGEX_MAP);
         instance.setCaseSensitive(false);
         instance.setRegex(true);
         instance.setTargetLang("en");
+        // note: default behaviour is 'replace on match'
+        //instance.setBehaviour(ValueMappingBehaviour.REPLACE_ON_MATCH);
 
         final ValueContext valueContext = SimpleValueContext.builder()
                 .values(ImmutableList.of(
@@ -287,7 +364,20 @@ public class ValueMapTransformerTest {
     }
 
     @Test
-    @Disabled
+    public void testValueMappingBehaviourEnum() {
+        assertEquals(ValueMappingBehaviour.ADD_ON_MATCH, ValueMappingBehaviour.fromValue("addOnMatch"));
+        assertEquals("addOnMatch", ValueMappingBehaviour.ADD_ON_MATCH.value());
+
+        assertEquals(ValueMappingBehaviour.REMOVE_ORIGINAL, ValueMappingBehaviour.fromValue("removeOriginal"));
+        assertEquals("removeOriginal", ValueMappingBehaviour.REMOVE_ORIGINAL.value());
+
+        assertEquals(ValueMappingBehaviour.REPLACE_ON_MATCH, ValueMappingBehaviour.fromValue("replaceOnMatch"));
+        assertEquals("replaceOnMatch", ValueMappingBehaviour.REPLACE_ON_MATCH.value());
+
+        assertThrows(IllegalArgumentException.class, () -> ValueMappingBehaviour.fromValue("somethingElse"));
+    }
+
+    @Test
     public void regexPerformanceTest() {
         final int valuesCount = 1_000_000;
 
@@ -296,10 +386,10 @@ public class ValueMapTransformerTest {
         instance.setRegex(false);
         instance.setTargetLang("en");
 
-        final ImmutableList.Builder<ValueLanguagePair> listBuilder 
+        final ImmutableList.Builder<ValueLanguagePair> listBuilder
                 = ImmutableList.<ValueLanguagePair>builder();
         final Random random = new Random();
-        for (int i = 0; i < valuesCount/2; i++) {
+        for (int i = 0; i < valuesCount / 2; i++) {
             listBuilder.add(new ValueLanguagePair(Integer.toString(random.nextInt()), "en"));
             listBuilder.add(new ValueLanguagePair("abc", "en"));
         }
