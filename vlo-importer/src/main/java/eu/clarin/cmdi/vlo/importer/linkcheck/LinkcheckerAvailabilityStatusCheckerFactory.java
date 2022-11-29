ce6cbbe5421e59efa2abd2f4cb8666c7cf6d1151
Twan Goosen <twan@clarin.eu>
2022-11-29 10:56:41 +0200

    distribution: got rid of 'fat jar' (refs #333)

--- vlo-importer/src/main/java/eu/clarin/cmdi/vlo/importer/linkcheck/LinkcheckerAvailabilityStatusCheckerFactory.java
+++ vlo-importer/src/main/java/eu/clarin/cmdi/vlo/importer/linkcheck/LinkcheckerAvailabilityStatusCheckerFactory.java
@@ -44,65 +44,67 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * @author CLARIN ERIC <clarin@clarin.eu>
  */
 public class LinkcheckerAvailabilityStatusCheckerFactory {
-    
+
     private final static Logger logger = LoggerFactory.getLogger(LinkcheckerAvailabilityStatusCheckerFactory.class);
     private AnnotationConfigApplicationContext ctx;
-    
+
     @PostConstruct
     public final void init() {
         // Programmatically initiate a Spring context
         logger.info("Initializing Spring context for LinkcheckerAvailabilityStatusChecker");
-        
+
         final ConfigurableEnvironment environment = new StandardEnvironment();
         configureEnvironment(environment.getPropertySources());
-        
+
         ctx = new AnnotationConfigApplicationContext();
         ctx.setEnvironment(environment);
         ctx.register(ApplicationConfig.class);
-        
+
         ctx.refresh();
     }
-    
+
     public final LinkcheckerAvailabilityStatusChecker getInstance() {
         if (ctx == null) {
             logger.error("Expecting a Spring context but none found.");
             throw new RuntimeException("Application context not available. Initialize factory first!");
         } else {
             logger.info("Requesting Spring bean for LinkcheckerAvailabilityStatusChecker");
-            return ctx.getBean(LinkcheckerAvailabilityStatusChecker.class);
+            final LinkcheckerAvailabilityStatusChecker instance = ctx.getBean(LinkcheckerAvailabilityStatusChecker.class);
+            logger.info("LinkcheckerAvailabilityStatusChecker instance retrieved from context: {}", instance);
+            return instance;
         }
     }
-    
+
     public void configureEnvironment(MutablePropertySources propertySources) {
         // can be overridden for configuration
     }
-    
+
     @SpringBootConfiguration
     @EnableAutoConfiguration
     @ComponentScan(basePackages = {"eu.clarin.linkchecker.persistence", "eu.clarin.cmdi.vlo.importer.linkcheck"})
     @EnableJpaRepositories(basePackages = "eu.clarin.linkchecker.persistence.repository")
     @EntityScan(basePackages = "eu.clarin.linkchecker.persistence.model")
     public static class ApplicationConfig {
-        
+
         @Autowired
         private StatusService statusService;
-        
+
         @Autowired
         private DataSource dataSource;
-        
+
         @Autowired
         private ConfigurableApplicationContext ctx;
-        
+
         @Bean
         public LinkcheckerAvailabilityStatusChecker checker() {
             return new LinkcheckerAvailabilityStatusChecker(statusService, statusWriter(), closeHandler());
         }
-        
+
         @Bean
         public Consumer<Writer> statusWriter() {
             return new LinkcheckerAvailabilityStatusCheckerStatusWriter(dataSource);
         }
-        
+
         @Bean
         public Callable closeHandler() {
             return () -> {
@@ -111,5 +113,5 @@ public class LinkcheckerAvailabilityStatusCheckerFactory {
             };
         }
     }
-    
+
 }
