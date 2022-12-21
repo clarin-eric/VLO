e787831630b5449c6c5f818b07d139da2c0dfb9a
Twan Goosen <twan@clarin.eu>
2022-12-21 18:26:45 +0100

    more xpath evaluation tests

--- vlo-5.0/vlo-mapping/src/test/java/eu/clarin/cmdi/vlo/mapping/impl/vtdxml/VTDValueContextTest.java
+++ vlo-5.0/vlo-mapping/src/test/java/eu/clarin/cmdi/vlo/mapping/impl/vtdxml/VTDValueContextTest.java
@@ -38,8 +38,33 @@ public class VTDValueContextTest {
 
         assertTrue(instance.matchesXPath(contextXPath), "canonical path");
         assertTrue(instance.matchesXPath("//cmd:Components/cmdp:OLAC-DcmiTerms/cmdp:date/text()"), "skip root");
-        assertTrue(instance.matchesXPath("//cmdp:OLAC-DcmiTerms/cmdp:date/text()"), "skip component root");
+        assertTrue(instance.matchesXPath("//cmdp:OLAC-DcmiTerms/cmdp:date/text()"), "skip root component");
+        assertTrue(instance.matchesXPath("//cmdp:*/cmdp:date/text()"), "skip components in namespace, using wildcard");
+        assertTrue(instance.matchesXPath("/cmd:CMD//cmdp:date/text()"), "skip intermediate");
         assertTrue(instance.matchesXPath("//cmdp:date/text()"), "skip all parents");
+        assertTrue(instance.matchesXPath("//node()"), "//node()");
+        assertTrue(instance.matchesXPath("//cmdp:date[@dcterms-type]/text()"), "select with attribute");
+        assertTrue(instance.matchesXPath("//cmdp:date[@dcterms-type=\"W3CDTF\"]/text()"), "select with attribute value");
+        assertTrue(instance.matchesXPath("//cmdp:date[@*]/text()"), "select with any attribute");
+    }
+    /**
+     * Test of matchesXPath method, of class ValueContextImpl.
+     */
+    @Test
+    public void testMatchesXPathTrueAttribute() {
+        final String contextXPath = "/cmd:CMD/cmd:Components/cmdp:OLAC-DcmiTerms/cmdp:format/@dcterms-type";
+        final VTDValueContext instance = createInstance("/records/p_1288172614026.cmdi", "clarin.eu:cr1:p_1288172614026", contextXPath);
+
+        assertTrue(instance.matchesXPath(contextXPath), "canonical path");
+        assertTrue(instance.matchesXPath("//cmd:Components/cmdp:OLAC-DcmiTerms/cmdp:format/@dcterms-type"), "skip root");
+        assertTrue(instance.matchesXPath("//cmdp:OLAC-DcmiTerms/cmdp:format/@dcterms-type"), "skip component root");
+        assertTrue(instance.matchesXPath("//cmdp:*/cmdp:format/@dcterms-type"), "skip component root, using wildcard");
+        assertTrue(instance.matchesXPath("/cmd:CMD//cmdp:format/@dcterms-type"), "skip intermediate");
+        assertTrue(instance.matchesXPath("//cmdp:format/@dcterms-type"), "skip all element parents");
+        assertTrue(instance.matchesXPath("//cmdp:*/@dcterms-type"), "element wilcard within namespace");
+        assertTrue(instance.matchesXPath("//cmdp:*/@*"), "element wilcard within namespace + attribute wildcard");
+        assertTrue(instance.matchesXPath("//@*"), "attribute wilcard");
+        assertTrue(instance.matchesXPath("//node()/@*"), "//node() + attribute wildcard");
     }
 
     /**
