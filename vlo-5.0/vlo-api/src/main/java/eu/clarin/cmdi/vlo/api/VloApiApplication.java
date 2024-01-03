5f07b31bd1d20853077f8975de631e71eaca47e4
Twan Goosen <twan@clarin.eu>
2024-01-03 14:27:39 +0100

    New vlo-api project

---
+++ vlo-5.0/vlo-api/src/main/java/eu/clarin/cmdi/vlo/api/VloApiApplication.java
@@ -0,0 +1,13 @@
+package eu.clarin.cmdi.vlo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VloApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VloApiApplication.class, args);
+	}
+
+}
---
+++ vlo-5.0/vlo-api/src/test/java/eu/clarin/cmdi/vlo/api/VloApiApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.clarin.cmdi.vlo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VloApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
