10b7220ddcb66eb5be0bdbd5376b934e2d62a54f
radudoros <radudoros@gmail.com>
2026-06-23 14:55:07 +0200

    Upgrade to solrj9x
    Fixed "suggest" response parsing
    Replaced solrj internal caching to Caffeine

--- vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/service/solr/impl/AutoCompleteServiceImpl.java
+++ vlo-web-app/src/main/java/eu/clarin/cmdi/vlo/service/solr/impl/AutoCompleteServiceImpl.java
@@ -3,54 +3,58 @@ package eu.clarin.cmdi.vlo.service.solr.impl;
 import eu.clarin.cmdi.vlo.service.solr.AutoCompleteService;
 import eu.clarin.cmdi.vlo.config.FieldNameService;
 import eu.clarin.cmdi.vlo.config.VloConfig;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrClient;
-import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.request.QueryRequest;
+import org.apache.solr.common.util.NamedList;
 
 /**
  * DAO that delivers suggestions for incomplete terms (autocomplete function)
  *
  * @author Thomas Eckart
  * @author Twan Goosen
- *
  */
 public class AutoCompleteServiceImpl extends SolrDaoImpl implements AutoCompleteService {
 
     public AutoCompleteServiceImpl(SolrClient solrClient, VloConfig config, FieldNameService fieldNameService) {
         super(solrClient, config, fieldNameService);
     }
-    
-    /**
-     * Returns list of suggestions for incomplete input (used for autocomplete
-     * function)
-     *
-     * @param input user input
-     * @return iterator over suggestions
-     */
+
     @Override
     public Iterator<String> getChoices(String input) {
-        if (input != null) {
-            
-            final SolrQuery query = new SolrQuery();
-            query.setQuery(input.toLowerCase());
-            query.setRequestHandler("/suggest");
-            
-            final QueryResponse response = fireQuery(query);
-            
-            if (response.getSuggesterResponse()!= null) {
-                final Map<String, List<String>> suggestions = response.getSuggesterResponse().getSuggestedTerms();
-                if (!suggestions.isEmpty()) {
-                    return suggestions.get(new ArrayList<>(suggestions.keySet()).get(0)).iterator();
-                }
-            }
+        if (input == null) {
+            return Collections.emptyIterator();
         }
+        final SolrQuery query = new SolrQuery();
+        query.setQuery(input.toLowerCase());
+        final QueryRequest req = new QueryRequest(query);
+        req.setPath("/suggest");
+        // fireRawQuery bypasses QueryResponse which crashes on the "suggest" key
+        // with solrj 9 + Solr 8 (LinkedHashMap vs NamedList cast)
+        return parseSuggestions(fireRawQuery(req));
+    }
 
-        return  Collections.emptyIterator();
+    private Iterator<String> parseSuggestions(NamedList<Object> raw) {
+        if (!(raw.get("suggest") instanceof Map<?, ?> suggest) || suggest.isEmpty()) {
+            return Collections.emptyIterator();
+        }
+        if (!(suggest.values().iterator().next() instanceof Map<?, ?> dict) || dict.isEmpty()) {
+            return Collections.emptyIterator();
+        }
+        if (!(dict.values().iterator().next() instanceof Map<?, ?> result)) {
+            return Collections.emptyIterator();
+        }
+        if (!(result.get("suggestions") instanceof List<?> suggestions)) {
+            return Collections.emptyIterator();
+        }
+        return suggestions.stream()
+                .filter(s -> s instanceof Map<?, ?> m && m.get("term") instanceof String)
+                .map(s -> (String) ((Map<?, ?>) s).get("term"))
+                .iterator();
     }
 }
